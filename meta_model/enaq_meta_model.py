38db75b9104dbe1b6902e89ba5f67f16d18bf335
lschmeling <lucas.schmeling@kehag.de>
2020-11-13 08:44:20 +0100

    Charge money for using the excess heat bus of ihs

--- meta_model/enaq_meta_model.py
+++ meta_model/enaq_meta_model.py
@@ -211,8 +211,9 @@ class ENaQMetaModel:
                 # For most ambient temperatures,
                 # the ice storage will melt (gain energy).
                 # So we add excess heat to allow not using it.
+                # We charge money for this to make it unattractive to use
                 s_ihs_excess = Sink(label="ihs_excess",
-                                    inputs={b_ihs: Flow()})
+                                    inputs={b_ihs: Flow(variable_costs=100)})
 
                 energy_system.add(b_ihs, s_ihs, s_ihs_excess)
 
