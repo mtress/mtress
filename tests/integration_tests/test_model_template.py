3ef2589dda4020da8bf7c5bd35bbea3f77ebec7b
Patrik Schönfeldt <patrik@sedna.snft.de>
2021-02-16 21:24:45 +0100

    Simplify test case some more

--- tests/integration_tests/test_model_template.py
+++ tests/integration_tests/test_model_template.py
@@ -8,6 +8,7 @@ SPDX-License-Identifier: MIT
 """
 import json
 import os
+import math
 import pandas as pd
 
 from oemof.solph import views, processing
@@ -15,11 +16,26 @@ from oemof.solph import views, processing
 from meta_model.enaq_meta_model import ENaQMetaModel
 
 
-def run_model_template():
+def run_model_template(custom_params=None):
+    if custom_params is None:
+        custom_params = {}
+
     dir_name = os.path.dirname(__file__)
     filename = os.path.join(dir_name, "empty_template.json")
     with open(filename) as f:
         params = json.load(f)
+
+    for key1 in custom_params:
+        if key1 not in params:
+            params[key1] = custom_params[key1]
+        else:
+            for key2 in custom_params[key1]:
+                if type(custom_params[key1][key2]) == dict:
+                    for key3 in custom_params[key1][key2]:
+                        params[key1][key2][key3] = (
+                            custom_params[key1][key2][key3])
+                else:
+                    params[key1][key2] = custom_params[key1][key2]
 
     params["demand"] = pd.DataFrame(
         params["demand"],
@@ -31,20 +47,14 @@ def run_model_template():
                            solver_io='lp',
                            cmdline_options={'ratio': 0.01})
 
+    meta_model.energy_system.results['main'] = views.convert_keys_to_strings(
+        processing.results(meta_model.model))
+
     return meta_model
 
 
 def test_empty_template():
     meta_model = run_model_template()
-
-    energy_system = meta_model.energy_system
-    energy_system.results['valid'] = True
-    energy_system.results['main'] = processing.results(
-        meta_model.model)
-    energy_system.results['main'] = views.convert_keys_to_strings(
-        energy_system.results['main'])
-    energy_system.results['meta'] = processing.meta_results(
-        meta_model.model)
 
     assert meta_model.thermal_demand() == 0
     assert meta_model.el_demand() == 0
