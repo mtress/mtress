29d8b1477b96b515c046c2a76e9f24a7f06a8239
Patrik Schönfeldt <patrik.schoenfeldt@dlr.de>
2021-03-31 15:46:52 +0200

    Fix post-hoc OPEX correction test

--- tests/integration_tests/test_exclusive_grid_connection.py
+++ tests/integration_tests/test_exclusive_grid_connection.py
@@ -93,7 +93,7 @@ def test_demand_supply_exclusive():
         abs_tol=HIGH_ACCURACY)
 
     op_costs = meta_model.operational_costs()
-    assert math.isclose(meta_model.operational_costs(),
+    assert math.isclose(op_costs,
                         electricity_costs(electricity_import,
                                           params,
                                           meta_model.time_range)
@@ -104,35 +104,23 @@ def test_demand_supply_non_exclusive():
     electricity_demand = np.full(3, 0.1)
     pv_generation = np.array([0.1, 0.5, 0])
     wind_generation = np.array([0.05, 0.2, 0.05])
-    pv_export = np.zeros(len(electricity_demand))
-    wind_export = np.zeros(len(electricity_demand))
-
-    # determination of pv and wind export
-    for i in range(0, len(electricity_demand)):
-        if electricity_demand[i] <= wind_generation[i]:
-            pv_export[i] = pv_generation[i]
-        else:
-            if wind_generation[i] + pv_generation[i] >= electricity_demand[i]:
-                pv_export[i] = wind_generation[i] + pv_generation[i] - electricity_demand[i]
-            else:
-                pv_export[i] = 0
-
-    for i in range(0, len(electricity_demand)):
-        if wind_generation[i] > electricity_demand[i]:
-            wind_export[i] = wind_generation[i] - electricity_demand[i]
 
     electricity_generation = pv_generation + wind_generation
     electricity_balance = electricity_generation - electricity_demand
     electricity_import = -electricity_balance
-    electricity_import[electricity_import < 0] = 0
+    electricity_import[electricity_import < 0] = 0  # [0, 0, 0.05]
     electricity_export = electricity_balance
-    electricity_export[electricity_export < 0] = 0
+    electricity_export[electricity_export < 0] = 0  # [0.05, 0.6, 0]
+
+    pv_export = np.minimum(electricity_export, pv_generation)  # [0.05, 0.5, 0]
+    remaining_excess = electricity_export - pv_generation
+    remaining_excess[remaining_excess < 0] = 0
+    wind_export = np.minimum(remaining_excess, wind_generation)  # [0, 0.1, 0]
 
     feed_in_subsidy = 1
     el_market_price = feed_in_subsidy/2
 
     feed_in_subsidy_wind = 0.8
-
 
     params = {
         "demand": {"electricity": electricity_demand},
@@ -142,7 +130,7 @@ def test_demand_supply_non_exclusive():
             "feed_in_subsidy": feed_in_subsidy,
             "spec_generation": pv_generation
         },
-        "wt": {
+        "wind_turbine": {
             "nominal_power": 1,
             "feed_in_subsidy": feed_in_subsidy_wind,
             "spec_generation": wind_generation
@@ -163,15 +151,16 @@ def test_demand_supply_non_exclusive():
 
     op_costs = meta_model.operational_costs(
         feed_in_order=[{"revenue": meta_model.pv_revenue,
-                        "flows": meta_model.pv_export_flows},
-                      {"revenue": meta_model.wt_revenue,
-                        "flows": meta_model.wt_export_flows}
+                        "flows": meta_model.pv_el_flows},
+                       {"revenue": meta_model.wt_revenue,
+                        "flows": meta_model.wt_el_flows}
                        ])
     el_costs = electricity_costs(electricity_import,
                                  params,
                                  meta_model.time_range)
     el_revenue = pv_export.sum() * feed_in_subsidy + wind_export.sum()*feed_in_subsidy_wind
-    assert math.isclose(op_costs, el_costs - el_revenue)
+    op_costs_manual = el_costs - el_revenue
+    assert math.isclose(op_costs, op_costs_manual)
 
 
 if __name__ == '__main__':
