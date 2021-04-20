313169852aa8593db081a787c197f41519f2f98a
lschmeling <lucas.schmeling@kehag.de>
2021-04-20 10:47:07 +0200

    Add new solar thermal bus for ice storage and add check if temperatures are present in meta model

--- mtress/meta_model.py
+++ mtress/meta_model.py
@@ -8,7 +8,6 @@ import pprint
 from oemof.solph import (Bus, EnergySystem, Flow, Sink, Source, Transformer,
                          Model, Investment, constraints, GenericStorage,
                          NonConvex)
-from oemof import thermal
 
 from .layered_heat import (HeatLayers, LayeredHeatPump, MultiLayerStorage,
                            HeatExchanger)
@@ -357,7 +356,9 @@ class MetaModel:
 
             energy_system.add(s_st, b_st)
 
-            if b_tgs:
+            if ('thermal_ground_storage' in kwargs
+                    and kwargs['thermal_ground_storage']['temperature']
+                    not in heat_layers.temperature_levels):
                 temp = tgs["temperature"]
                 temp_str = "{0:.0f}".format(temp)
                 st_level_label = 't_st_' + temp_str
@@ -371,6 +372,23 @@ class MetaModel:
 
                 self.solar_thermal_th_flows.append((st_level_label,
                                                     b_tgs.label))
+                energy_system.add(t_st_level)
+
+            if ('ice_storage' in kwargs
+                    and 0 not in heat_layers.temperature_levels):
+                temp = 0
+                temp_str = "{0:.0f}".format(temp)
+                st_level_label = 't_st_' + temp_str
+                t_st_level = Transformer(
+                    label=st_level_label,
+                    inputs={b_st: Flow(nominal_value=st["area"])},
+                    outputs={b_ihs: Flow(nominal_value=1)},
+                    conversion_factors={
+                        b_st: (1 / st['spec_generation'][
+                            'ST_' + str(temp)]).to_list()})
+
+                self.solar_thermal_th_flows.append((st_level_label,
+                                                    b_ihs.label))
                 energy_system.add(t_st_level)
 
             for temp in heat_layers.temperature_levels:
