3b51d29f4f3ca190bd3a7589c270fe7a9719741e
Patrik Schönfeldt <patrik.schoenfeldt@dlr.de>
2021-05-25 09:31:27 +0200

    Allow run_mtress to run with shortened time periods
    Before, the time range from the CSV files was strictly used.
    Now, integer indexes can be used to look into sub-periods.

--- mtress/run_mtress.py
+++ mtress/run_mtress.py
@@ -8,24 +8,27 @@ import pandas as pd
 from mtress import MetaModel
 
 
-def _read_csv_files(parameter_dict, dir_path):
+def _read_csv_files(parameter_dict, dir_path, time_range):
     for key in parameter_dict:
         item = parameter_dict[key]
         if type(item) == dict:
-            _read_csv_files(parameter_dict[key], dir_path)
+            _read_csv_files(parameter_dict[key], dir_path, time_range)
         elif type(item) == str and ".csv:" in item:
             file_name, column_name = item.split(":", 1)
             csv_data = pd.read_csv(os.path.join(dir_path, file_name),
                                    comment='#', index_col=0,
                                    sep=',',
                                    parse_dates=True)
+            csv_data = csv_data.iloc[time_range[0]:time_range[1]]
             parameter_dict[key] = csv_data[column_name]
 
 
 def run_mtress(json_file,
+               time_range=(0, -1),
                solver="cbc"):
     """
     :param json_file: json file holding configuration
+    :param time_range: tuple (first time step, last time step)
     :param solver: solver to use for oemof.solph
     """
     with open(json_file) as f:
@@ -33,7 +36,7 @@ def run_mtress(json_file,
 
     dir_path = os.path.dirname(os.path.realpath(json_file))
 
-    _read_csv_files(parameters, dir_path)
+    _read_csv_files(parameters, dir_path, time_range=time_range)
 
     meta_model = MetaModel(**parameters)
     meta_model.model.solve(solver=solver,
