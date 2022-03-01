12f4dbc448f0ebd70c049a4cda57b489bfbbee01
Patrik Schönfeldt <patrik.schoenfeldt@dlr.de>
2022-03-01 15:35:41 +0100

    Delete layered_heat from __init__

--- mtress/__init__.py
+++ mtress/__init__.py
@@ -9,14 +9,12 @@ SPDX-License-Identifier: MIT
 
 __version__ = "3.0.0dev0"
 
-from . import layered_heat
 from . import physics
 from . import technologies
 from ._meta_model import MetaModel
 from ._run_mtress import run_mtress
 
 __all__ = [
-    "layered_heat",
     "physics",
     "technologies",
     "MetaModel",
