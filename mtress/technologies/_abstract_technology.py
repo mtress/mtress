99e6529e6116fc508fa425be9cf9e3b32752c091
Sunke Schlüters <sunke.schlueters@dlr.de>
2022-08-05 09:35:46 +0200

    Move flow categorization to AbstractTechnology

--- mtress/technologies/_abstract_technology.py
+++ mtress/technologies/_abstract_technology.py
@@ -1,14 +1,33 @@
 """Base class for MTRESS technologies."""
 
 from abc import ABC, abstractmethod
+from enum import Enum, auto
 
 from oemof import solph
 
 from .._abstract_component import AbstractComponent
 
 
+class FlowType(Enum):
+    """Types/categories for oemof flows."""
+
+    ALL = auto()
+    IN = auto()
+    OUT = auto()
+    PRODUCTION = auto()
+    STORAGE = auto()
+    EXPORT = auto()
+    IMPORT = auto()
+    RENEWABLE = auto()
+
+
 class AbstractTechnology(AbstractComponent):
     """Base class for MTRESS technologies."""
+
+    def __init__(self, **kwargs):
+        """Initialize technology."""
+        super().__init__(**kwargs)
+        self._flows = {flow_type: set() for flow_type in FlowType}
 
     def add_interconnections(self):
         """
@@ -28,6 +47,26 @@ class AbstractTechnology(AbstractComponent):
         not be created prior to the model generation.
         """
 
+    def categorise_flow(self, flow, flow_types):
+        """Categorise given flow under the named flow_types."""
+        for flow_type in flow_types | {FlowType.ALL}:
+            if flow_type in self._flows:
+                self._flows[flow_type].add(flow)
+            else:
+                self._flows[flow_type] = {flow}
+
+    def get_flows(self, flow_types):
+        """Return flows categorised under all named flow_types."""
+        flows = self._flows[FlowType.ALL].copy()
+
+        for flow_type in flow_types:
+            try:
+                flows.intersection_update(self._flows[flow_type])
+            except KeyError:
+                return set()
+
+        return flows
+
 
 class AbstractAnergySource(ABC):
     """Base class for anergy providing technologies."""
@@ -36,10 +75,8 @@ class AbstractAnergySource(ABC):
     @abstractmethod
     def temperature(self):
         """Return anergy temperature level."""
-        pass
 
     @property
     @abstractmethod
     def bus(self):
         """Return oemof bus to connect to."""
-        pass
