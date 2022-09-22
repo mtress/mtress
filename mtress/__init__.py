b2d3b78ad8af55957db0f1086d5c34714f69899d
Sunke Schlüters <sunke.schlueters@dlr.de>
2022-09-22 14:52:11 +0200

    Remove commented code

--- mtress/__init__.py
+++ mtress/__init__.py
@@ -9,22 +9,6 @@ SPDX-License-Identifier: MIT
 
 __version__ = "3.0.0dev0"
 
-# from . import physics
-# from . import technologies
-# from ._meta_model import MetaModel
-# from ._run_mtress import (
-#     prepare_mtress_config,
-#     run_mtress,
-# )
-
-# __all__ = [
-#     "physics",
-#     "technologies",
-#     "MetaModel",
-#     "prepare_mtress_config",
-#     "run_mtress",
-# ]
-
 from ._location import Location
 from ._meta_model import MetaModel
 
