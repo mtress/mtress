cf6f969bf0c0b07be767a2ebec42a3e1442cd137
Patrik Schönfeldt <patrik@salacia.snft.de>
2022-10-12 08:33:52 +0200

    Reduce time steps in electricity example to 3

--- examples/electricity_only.py
+++ examples/electricity_only.py
@@ -1,18 +1,18 @@
 
 
-from mtress import carriers, demands, Location, MetaModel, technologies
+from mtress import carriers, demands, Location, MetaModel
 
 meta_model = MetaModel(time_index={
     "start": "2021-07-10 00:00:00",
-    "end": "2021-07-10 05:00:00",
+    "end": "2021-07-10 02:00:00",
     "freq": "60T",
 })
 
 house_1 = Location(meta_model=meta_model, name="house_1")
 
-carrier_el = carriers.Electricity(location=house_1, costs={"working_price": 35, "demand_rate": 0})
+carriers.Electricity(location=house_1, costs={"working_price": 35, "demand_rate": 0})
 
-demand_el = demands.Electricity(location=house_1, time_series=[0, 1, 2, 3, 2, 1, 0])
+demands.Electricity(location=house_1, time_series=[0, 0.5, 9])
 
-meta_model.solve()
-
+solved_model = meta_model.solve(solve_kwargs={"tee": True})
+solved_model.write('electricity_only.lp', io_options={'symbolic_solver_labels': True})
