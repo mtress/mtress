05f1f27904e91f65e0ed891c6f2773d870307de8
Keno Oltmanns <keno.oltmanns@dlr.de>
2022-11-25 18:26:51 +0100

    Added final documentation/explanation for both working examples.

--- examples/electricity_only.py
+++ examples/electricity_only.py
@@ -1,6 +1,19 @@
 
 
 from mtress import carriers, demands, Location, MetaModel
+"""
+    Basic working 'electricity and heat' example which includes a location (house),
+    an electricity carrier which acts as a electricity source/supply from the 
+    official grid (working price of 35 ct/kWh) as well as a demand (consumer)
+    with a demand time series.
+    
+    At first an energy system (here meta_model) is defined with a time series (index).
+    Afterwards a location is defined and added to the energysystem. Then the 
+    electricity carrier and demand (time series) are added to the energysystem. 
+    Finally, the energy system is optimised/solved via meta_model.solve and the 
+    solver output is written to an .lp file.   
+"""
+
 
 meta_model = MetaModel(time_index={
     "start": "2021-07-10 00:00:00",
