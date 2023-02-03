6477ebb9ae8d64d5d72f68806b5759352eb82621
Sunke Schlüters <sunke.schlueters@dlr.de>
2023-02-03 13:10:39 +0100

    Add new solph interface to the generic renewable energy source

--- mtress/technologies/_renewable_electricity_source.py
+++ mtress/technologies/_renewable_electricity_source.py
@@ -5,39 +5,59 @@ basic heat layer functionality
 
 SPDX-FileCopyrightText: Deutsches Zentrum für Luft und Raumfahrt
 SPDX-FileCopyrightText: Patrik Schönfeldt
+SPDX-FileCopyrightText: Sunke Schlüters
+
 
 SPDX-License-Identifier: MIT
 """
 
-from oemof import solph
+from oemof.solph import Flow
+from oemof.solph.components import Source
 
+from .._abstract_component import AbstractSolphComponent
+from .._data_handler import TimeseriesSpecifier
 from ..carriers import Electricity
-
-from ._abstract_technology import (
-    AbstractTechnology,
-)
+from ._abstract_technology import AbstractTechnology
 
 
-class RenewableElectricitySource(AbstractTechnology):
-    """
-    a generic electricity source
-    """
+class RenewableElectricitySource(AbstractTechnology, AbstractSolphComponent):
+    """A generic electricity source."""
 
     def __init__(
         self,
-        location,
-        name,
-        nominal_power,
-        specific_generation,
+        name: str,
+        nominal_power: float,
+        specific_generation: TimeseriesSpecifier,
+        fixed: bool = True,
     ):
-        super().__init__(location=location, name=name)
+        """
+        Initialize generic electricity source.
 
-        # Add electrical connection
+        :param nominal_power: Nominal power of the source.
+        :param specific_generation: Timeseries of generated power (values in [0,1]).
+        :param fixed: Indicate if the generation is fixed to the values defined by
+            nominal_power and specific_generation or bounded by these values.
+        """
+        super().__init__(name)
+
+        self.nominal_power = nominal_power
+        self.specific_generation = specific_generation
+
+        self.fixed = fixed
+
+    def build_core(self):
+        """Build oemof solph core structure."""
         electricity_carrier = self.location.get_carrier(Electricity)
 
-        self._electricity_bus = electricity_bus = solph.Bus(
-            label=self._generate_label("electricity"),
-            inputs={electricity_carrier.production: solph.Flow()},
+        flow = (
+            Flow(nominal_value=self.nominal_power, fix=self.specific_generation)
+            if self.fixed
+            else Flow(nominal_value=self.nominal_power, max=self.specific_generation)
         )
 
-        self.location.energy_system.add(electricity_bus)
+        self._solph_model.add_solph_component(
+            mtress_component=self,
+            label="source",
+            solph_component=Source,
+            outputs={electricity_carrier.production: flow},
+        )
