4d977c31776355c4a1b67b2cfb22f3dc3d984d11
Patrik Schönfeldt <Patrik.Schoenfeldt@dlr.de>
2023-02-13 11:40:17 +0100

    Add FullyMixedHeatStorage

--- mtress/technologies/__init__.py
+++ mtress/technologies/__init__.py
@@ -11,11 +11,12 @@ from ._air_heat_exchanger import AirHeatExchanger
 from ._h2_compressor import H2Compressor
 from ._h2_electrolyzer import PEMElectrolyzer
 from ._heat_pump import HeatPump
-from ._heat_storage import LayeredHeatStorage
+from ._heat_storage import FullyMixedHeatStorage, LayeredHeatStorage
 from ._photovoltaics import Photovoltaics
 
 __all__ = [
     "AirHeatExchanger",
+    "FullyMixedHeatStorage",
     "H2Compressor",
     "PEMElectrolyzer",
     "HeatPump",
