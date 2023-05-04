315f5d62e4a19f62d127dbfcc776a86b020f57cf
Ajay Upadhaya <ajay.upadhaya@dlr.de>
2023-05-04 15:54:52 +0200

    Working example of hydrogen production and compressed h2 to demand and waste heat utilisation.

--- examples/hydrogen_production.py
+++ examples/hydrogen_production.py
@@ -1,8 +1,9 @@
-"""Example to illustrate hydrogen production via Electrolyser and compression to meet hydrogen demand of the Refueling Station"""
+"""Example to illustrate hydrogen production via Electrolyser and compression to meet
+   hydrogen demand of the Refueling Station"""
 
 from mtress import Location, MetaModel, SolphModel, carriers, demands, technologies
 from mtress._helpers._visualization import generate_graph
-
+from oemof.solph.processing import results
 
 energy_system = MetaModel()
 
@@ -11,29 +12,47 @@ energy_system.add_location(house_1)
 
 house_1.add_carrier(carriers.Electricity(working_rate=35, demand_rate=0))
 
-house_1.add_demand(demands.Hydrogen(name="H2_demand",time_series=[2], pressure= 250))
+house_1.add_demand(demands.Hydrogen(name="H2_demand", time_series=[4, 5, 5, 10], pressure=100))
 
+house_1.add_demand(
+    demands.Electricity(
+        name="electricity demand",
+        time_series=[32, 12, 12, 34],
+    )
+)
 
 house_1.add_carrier(
     carriers.Hydrogen(
-        pressure_levels=[30, 250],
+        pressure_levels=[30, 100],
     ))
-"""house_1.add_carrier(
+
+house_1.add_carrier(
     carriers.Heat(
-        temperature_levels=[20, 30, 55],
+        temperature_levels=[45],
         reference_temperature=10,
-    ))"""
+    ))
 
+house_1.add_demand(
+    demands.FixedTemperatureHeat(
+        name="hot water",
+        flow_temperature=45,
+        return_temperature=10,
+        time_series=[55, 75, 85, 13],
+    )
+)
 
-house_1.add_technology(technologies.PEMElectrolyzer(name="Electrolyser",nominal_power=5))
+house_1.add_technology(technologies.PEMElectrolyzer(name="Ely",nominal_power=500))
+house_1.add_technology(technologies.HeatPump(name="hp0", thermal_power_limit=None))
+house_1.add_technology(technologies.H2Compressor(name='H2Compr', nominal_power=5))
 
-house_1.add_technology(technologies.H2Compressor(name='H2Compressor',nominal_power= 0.5))
-
+house_1.add_technology(
+    technologies.AirHeatExchanger(name="ahe", air_temperatures=[3, 6, 13, 12])
+)
 solph_representation = SolphModel(
     energy_system,
     timeindex={
         "start": "2021-07-10 00:00:00",
-        "end": "2021-07-10 00:00:00",
+        "end": "2021-07-10 03:00:00",
         "freq": "60T",
     },
 )
@@ -43,7 +62,8 @@ solph_representation.build_solph_model()
 
 solved_model = solph_representation.solve(solve_kwargs={"tee": True})
 
-plot = generate_graph(solved_model.es)
-plot.render(format="png", renderer="cairo", formatter="gdiplus")
+plot = solph_representation.graph(detail=False)
+plot.render(outfile="hydrogen_heat_simple.png")
 
+myresults= results(solved_model)
 solved_model.write("hydrogen_production.lp", io_options={"symbolic_solver_labels": True})
