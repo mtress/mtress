008934393bea3a076d69713a75c0a8588823f80e
Patrik Schönfeldt <patrik@salacia.snft.de>
2023-05-16 20:19:53 +0200

    Add tests for Location.add(...)

--- tests/unit_tests/test_location.py
+++ tests/unit_tests/test_location.py
@@ -5,7 +5,7 @@ Tests for the MTRESS class Location
 
 from typing import Iterable
 
-from mtress import Location
+from mtress import Location, carriers, demands
 
 def test_basic_initialisation():
     name = "house_1"
@@ -13,3 +13,49 @@ def test_basic_initialisation():
 
     assert house_1.name == "house_1"
     assert isinstance(house_1.components, Iterable)
+
+
+def test_add_carrier():
+    name = "house_1"
+    house_1 = Location(name=name)
+
+    carrier1 = carriers.Electricity(working_rate=0.5)
+    carrier2 = carriers.Electricity(working_rate=0.4)
+
+    house_1.add_carrier(carrier1)
+    assert house_1.get_carrier(carriers.Electricity) == carrier1
+
+    # carrier2 replaces carrier1
+    house_1.add(carrier2)
+    assert house_1.get_carrier(carriers.Electricity) == carrier2
+
+
+def test_add_component():
+    name = "house_1"
+    house_1 = Location(name=name)
+
+    carrier0 = carriers.Electricity()
+    carrier1 = carriers.Electricity()
+    demand1 = demands.Electricity(name="demand1", time_series=[0,1,2])
+    demand2 = demands.Electricity(name="demand2", time_series=[1,2,3])
+
+    house_1.add(carrier0)
+    house_1.add(carrier1)
+    house_1.add(demand1)
+    house_1.add(demand2)
+
+    # carriers are not returned by get_technology
+    assert carrier0 not in house_1.get_technology(carriers.Electricity)
+    assert carrier1 not in house_1.get_technology(carriers.Electricity)
+
+    # demands are returned by get_technology
+    assert demand1 in house_1.get_technology(demands.Electricity)
+    assert demand2 in house_1.get_technology(demands.Electricity)
+
+    # carrier0 is overwitten by carrier1
+    assert carrier0 not in house_1.components
+
+    # all other components are in this Iterable
+    assert carrier1 in house_1.components
+    assert demand1 in house_1.components
+    assert demand2 in house_1.components
