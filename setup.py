6d1f2a0c3a9b027982843faa89572128b02fc69a
Patrik Schönfeldt <github@patrikschoenfeldt.de>
2023-06-19 20:37:13 +0200

    Set upper limit for solph version
    Solph v0.5 introduces relevant API breaking changes. MTRESS 2.1.0 does not support these.

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ setup(
     zip_safe=False,
     install_requires=[
         "pyyaml >= 6.0",
-        "oemof.solph >= 0.4.4",
+        "oemof.solph >= 0.4.4, <0.5",
         "oemof.thermal >= 0.0.5",
     ],
 )
