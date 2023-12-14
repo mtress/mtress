1cd3ead8ae321aab20cdaccdd438499378ed2ce7
Diana Maldonado <Diana.Maldonado@dlr.de>
2023-12-14 08:15:36 +0100

    unit tests

---
+++ tests/unit_tests/test_technologies/test_photovoltaics.py
@@ -0,0 +1,9 @@
+from mtress.technologies import Photovoltaics
+
+
+def test_pv_initialisation():
+    pv_name = "test_pv"
+    pv_location = 0.5
+    pv_nominal_power = 0.8
+    pv_power_limit = 2
+    ambient_temperature = 10
