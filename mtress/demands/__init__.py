3f5b3e7e897308a7156c91d7510285dac35b633e
Maldonado Castro, Diana Teresa <diana.maldonado@dlr.de>
2024-06-06 08:58:34 +0200

    Allow cooling demand

--- mtress/demands/__init__.py
+++ mtress/demands/__init__.py
@@ -2,6 +2,7 @@
 
 from ._electricity import Electricity
 from ._fixed_temperature_heat import FixedTemperatureHeat
+from ._fixed_temperature_heat_cool import FixedTemperatureHeatCool
 from ._heat_sink import HeatSink
 from ._gas import GasDemand
 
@@ -10,4 +11,5 @@ __all__ = [
     "FixedTemperatureHeat",
     "GasDemand",
     "HeatSink",
+    "FixedTemperatureHeatCool",
 ]
---
+++ mtress/demands/_fixed_temperature_heat_cool.py
@@ -0,0 +1,162 @@
+"""Room heating technologies."""
+
+from oemof.solph import Bus, Flow
+from oemof.solph.components import Source, Sink, Converter
+
+from .._abstract_component import AbstractSolphRepresentation
+from .._data_handler import TimeseriesType
+from ..carriers import HeatCarrier
+from ._abstract_demand import AbstractDemand
+
+
+class FixedTemperatureHeatCool(AbstractDemand, AbstractSolphRepresentation):
+    """
+    Space heating with a fixed flow and return temperature.
+
+    Takes energy from the flow temperature level and returns energy at the lower return
+    temperature level.
+
+      (Q(T2))
+              ↘
+              [heater1,2] ───→ (Qdemand)
+              ↙
+      (Q(T1))
+
+
+    Functionality: Demands contain time series of energy that is needed.
+        The heat demand automatically connects to its corresponding
+        heat  carrier. A name identifying the demand has to be given that
+        is unique for the location, because multiple demands of one type
+        can exist for one location. Also, the heat demand needs a
+        specified temperature level.
+
+    Procedure: Create a simple heat demand by doing the following:
+
+            house_1.add(demands.FixedTemperatureHeat(
+                flow_temperature=30, # in °C
+                return_temperature=20, # in °C
+                time_series=[50]))
+
+    Notice: While energy from electricity and the gaseous carriers is
+     just consumed, heat demands have a returning energy flow.
+    """
+
+    def __init__(
+        self,
+        name: str,
+        flow_temperature: float,
+        return_temperature: float,
+        time_series,
+        sink: bool = True,
+    ):
+        """
+        Initialize space heater.
+
+        :param flow_temperature: Flow temperature
+        :param return_temperature: Return temperature
+        """
+        super().__init__(name=name)
+
+        if not flow_temperature > return_temperature:
+            raise ValueError("Flow must be higher than return temperature")
+
+        self.flow_temperature = flow_temperature
+        self.return_temperature = return_temperature
+
+        self._time_series = time_series
+        self.sink = sink
+
+    def build_core(self):
+        """Build core structure of oemof.solph representation."""
+        carrier = self.location.get_carrier(HeatCarrier)
+
+        if self.flow_temperature not in carrier.levels:
+            raise ValueError("Flow temperature must be a temperature level")
+
+        if self.return_temperature not in carrier.levels:
+            raise ValueError("Return temperature must be a temperature level")
+
+        temperature_ratio = 0
+        inputs = {}
+        outputs = {}
+        conversion_factors = {}
+
+        if self.sink == True:
+            output = self.create_solph_node(
+                label="output",
+                node_type=Bus,
+            )
+            outputs[output] = Flow()
+
+            inputs[carrier.level_nodes[self.flow_temperature]] = Flow()
+            outputs[carrier.level_nodes[self.return_temperature]] = Flow()
+
+            temperature_ratio = (self.return_temperature - carrier.reference) / (
+                self.flow_temperature - carrier.reference
+            )
+            conversion_factors = {
+                carrier.level_nodes[self.flow_temperature]: 1,
+                output: 1 - temperature_ratio,
+                carrier.level_nodes[self.return_temperature]: temperature_ratio,
+            }
+
+            self.create_solph_node(
+                label="heat_exchanger",
+                node_type=Converter,
+                inputs=inputs,
+                outputs=outputs,
+                conversion_factors=conversion_factors,
+            )
+
+            self.create_solph_node(
+                label="sink",
+                node_type=Sink,
+                inputs={
+                    output: Flow(
+                        nominal_value=1,
+                        fix=self._solph_model.data.get_timeseries(
+                            self._time_series, kind=TimeseriesType.INTERVAL
+                        ),
+                    )
+                },
+            )
+        else:
+            input = self.create_solph_node(
+                label="input",
+                node_type=Bus,
+            )
+
+            inputs[input] = Flow()
+
+            outputs[carrier.level_nodes[self.flow_temperature]] = Flow()
+            inputs[carrier.level_nodes[self.return_temperature]] = Flow()
+
+            temperature_ratio = (self.return_temperature - carrier.reference) / (
+                self.flow_temperature - carrier.reference
+            )
+            conversion_factors = {
+                carrier.level_nodes[self.flow_temperature]: 1,
+                input: 1 - temperature_ratio,
+                carrier.level_nodes[self.return_temperature]: temperature_ratio,
+            }
+
+            self.create_solph_node(
+                label="heat_exchanger",
+                node_type=Converter,
+                inputs=inputs,
+                outputs=outputs,
+                conversion_factors=conversion_factors,
+            )
+
+            self.create_solph_node(
+                label="Source",
+                node_type=Source,
+                outputs={
+                    input: Flow(
+                        nominal_value=1,
+                        fix=self._solph_model.data.get_timeseries(
+                            self._time_series, kind=TimeseriesType.INTERVAL
+                        ),
+                    )
+                },
+            )
