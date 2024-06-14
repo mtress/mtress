a4204cd2feb9c3844ab96a0db3678f0075ad86d9
“mald_di” <diana.maldonado@dlr.de>
2024-06-14 09:39:29 +0200

    Add Hands-on session folder

---
+++ examples/hands-on session/3_heat_pump.py
@@ -0,0 +1,79 @@
+import os
+import pandas as pd
+from oemof.solph.processing import results
+
+from mtress import Location, MetaModel, SolphModel, carriers, demands, technologies
+from mtress._helpers import get_flows
+
+os.chdir(os.path.dirname(__file__))
+
+energy_system = MetaModel()
+
+house_1 = Location(name="house_1")
+energy_system.add_location(house_1)
+
+# Add carrier
+house_1.add(carriers.Electricity())
+house_1.add(technologies.ElectricityGridConnection(working_rate=35))
+
+house_1.add(
+    carriers.HeatCarrier(
+        temperature_levels=[5, 10, 20, 30, 40],
+        reference_temperature=0,
+    )
+)
+
+# Add technologies
+house_1.add(
+    technologies.HeatExchanger(
+        name="Air_HE",
+        reservoir_temperature=20,
+        maximum_temperature=30,
+        minimum_temperature=10,
+    )
+)
+
+house_1.add(
+    technologies.HeatPump(
+        name="HeatPump",
+        thermal_power_limit=None,
+        max_temp_primary=10,
+        min_temp_primary=5,
+        max_temp_secondary=30,
+        min_temp_secondary=20,
+    )
+)
+
+# Add demands
+house_1.add(
+    demands.FixedTemperatureHeatCool(
+        name="Cooling_demand",
+        flow_temperature=10,
+        return_temperature=5,
+        time_series=[50, 50],
+        sink=False,
+    )
+)
+
+solph_representation = SolphModel(
+    energy_system,
+    timeindex={
+        "start": "2021-07-10 00:00:00",
+        "end": "2021-07-10 02:00:00",
+        "freq": "60T",
+    },
+)
+
+solph_representation.build_solph_model()
+
+plot = solph_representation.graph(detail=True)
+plot.render(outfile="heat_pump_cooling_detail.png")
+
+plot = solph_representation.graph(detail=False)
+plot.render(outfile="heat_pump_cooling_simple.png")
+
+solved_model = solph_representation.solve(solve_kwargs={"tee": True})
+myresults = results(solved_model)
+flows = get_flows(myresults)
+
+solved_model.write("heat.lp", io_options={"symbolic_solver_labels": True})
