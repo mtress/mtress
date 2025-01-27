819ce7ad663b2cf94998d6d720438647de428321
Ellermann <julius.ellermann@dlr.de>
2025-01-27 15:45:09 +0100

    init slack component, currently only electricity

---
+++ src/mtress/technologies/_slack.py
@@ -0,0 +1,71 @@
+# -*- coding: utf-8 -*-
+
+"""
+Renewable energy source
+
+SPDX-FileCopyrightText: Deutsches Zentrum für Luft und Raumfahrt
+SPDX-FileCopyrightText: Julius Ellermann
+
+
+SPDX-License-Identifier: MIT
+"""
+
+from oemof.solph import Bus, Flow
+from oemof.solph.components import Source, Sink
+
+from .._abstract_component import AbstractSolphRepresentation
+from .._data_handler import TimeseriesSpecifier, TimeseriesType
+from ..carriers import ElectricityCarrier, HeatCarrier, GasCarrier
+from ._abstract_technology import AbstractTechnology
+
+
+class Slack(AbstractTechnology, AbstractSolphRepresentation):
+    """A component that provides sink and source slack nodes"""
+
+    def __init__(self, name: str = "slack", penalty: float = 1e9):
+        super().__init__(name=name)
+        self.penalty = penalty
+
+    def build_core(self):
+        """Build oemof solph core structure."""
+        # get all carriers
+        try:
+            electricity_carrier = self.location.get_carrier(ElectricityCarrier)
+        except KeyError:
+            electricity_carrier = None
+        try:
+            heat_carrier = self.location.get_carrier(HeatCarrier)
+        except KeyError:
+            heat_carrier = None
+        try:
+            gas_carrier = self.location.get_carrier(GasCarrier)
+        except KeyError:
+            gas_carrier = None
+
+        slack_source = {}  # missing energy
+        slack_sink = {}  # excess energy
+
+        # collect all nodes to connect to
+        # electricity: distribution node
+        if electricity_carrier is not None:
+            node = electricity_carrier.distribution
+            slack_source[node] = Flow(variable_costs=self.penalty)
+            slack_sink[node] = Flow(variable_costs=self.penalty)
+
+        # heat: all T_?? nodes
+
+        # gas: highest (missing) and lowest (excess) pressure per gas
+
+        # create slack nodes
+        self.create_solph_node(
+            label="missing_energy",
+            node_type=Source,
+            outputs=slack_source,
+        )
+
+        self.create_solph_node(
+            label="excess_energy",
+            node_type=Sink,
+            inputs=slack_sink,
+        )
+        # TODO: remove missing / excess heat from heat carrier
