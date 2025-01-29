1df8dc2799773d26975c145c66ffd3bede5058e9
Pedro Lacerda de Magalhães <pedro.magalhaes@dlr.de>
2025-01-29 17:27:32 +0100

    Adds tests to the CHP class and respective templates.

--- tests/unit_tests/test_technologies/test_chp.py
+++ tests/unit_tests/test_technologies/test_chp.py
@@ -1,23 +1,217 @@
-from mtress.physics import NATURAL_GAS
-from mtress.technologies import CHP, NATURALGAS_CHP
+import math
+import pytest
+import os
+import pyomo.environ as pyo
+from oemof.solph.processing import meta_results
+from mtress import (
+    Location,
+    MetaModel,
+    SolphModel,
+    carriers,
+    demands,
+    technologies,
+)
+# from mtress.physics import NATURAL_GAS, BIOGAS, BIO_METHANE, HYDROGEN
+from mtress.technologies._chp import CHPTemplate
+from mtress.technologies import (
+    CHP,
+    # OffsetCHP,
+    # templates
+    NATURALGAS_CHP,
+    BIOGAS_CHP,
+    BIOMETHANE_CHP,
+    HYDROGEN_CHP,
+    HYDROGEN_MIXED_CHP,
+)
 
+class TestCHP:
 
-def test_fuelcell():
-    chp_name = ("CHP",)
-    chp_nominal_power = 100e3
+    def check_chp_template(self, chp: CHP, template: CHPTemplate):
 
-    # Considering NATURALGAS_CHP template type
-    chp = CHP(
-        name=chp_name,
-        nominal_power=chp_nominal_power,
-        template=NATURALGAS_CHP,
+        assert chp.electric_efficiency == template.electric_efficiency
+        assert chp.thermal_efficiency == template.thermal_efficiency
+        # assert chp.min_load_electrical_efficiency == template.min_load_electrical_efficiency
+        # assert chp.min_load_thermal_efficiency == template.min_load_thermal_efficiency
+        # assert chp.full_load_electrical_efficiency == template.full_load_electrical_efficiency
+        # assert chp.full_load_thermal_efficiency == template.full_load_thermal_efficiency
+        # assert chp.min_load_electrical_efficiency == template.min_load_electrical_efficiency
+        # assert chp.min_load_thermal_efficiency == template.min_load_thermal_efficiency
+        assert chp.maximum_temperature == template.maximum_temperature
+        assert chp.minimum_temperature == template.minimum_temperature
+        assert chp.input_pressure == template.input_pressure
+        assert chp.gas_type == template.gas_type
+        assert type(chp.gas_type) is dict
+
+    @pytest.mark.parametrize(
+        "template, expected_result",
+        [(NATURALGAS_CHP, 0.8508048112500001), 
+         (BIOGAS_CHP, 58187714401.612885), 
+         (BIOMETHANE_CHP,  0.7855700564999999), 
+         (HYDROGEN_CHP,  0.32534810449999996), 
+         (HYDROGEN_MIXED_CHP,  1307692350000.05)],
     )
+    def test_chp(self, template: CHPTemplate, expected_result: float):
 
-    assert chp.name == chp_name
-    assert chp.nominal_power == chp_nominal_power
-    assert chp.electric_efficiency == NATURALGAS_CHP.electric_efficiency
-    assert chp.thermal_efficiency == NATURALGAS_CHP.thermal_efficiency
-    assert chp.maximum_temperature == NATURALGAS_CHP.maximum_temperature == 85
-    assert chp.input_pressure == NATURALGAS_CHP.input_pressure == 1
-    assert chp.gas_type == {NATURAL_GAS: 1}
-    assert type(chp.gas_type) is dict
+        os.chdir(os.path.dirname(__file__))
+        energy_system = MetaModel()
+        house_1 = Location(name="house_1")
+        energy_system.add_location(house_1)
+
+        house_1.add(carriers.ElectricityCarrier())
+        house_1.add(technologies.ElectricityGridConnection(working_rate=50e-6))
+
+        house_1.add(
+            carriers.GasCarrier(
+                gases={
+                    gas: [template.input_pressure]
+                    for gas, share in template.gas_type.items()
+                }
+            )
+        )
+        for gas, share in template.gas_type.items():
+            house_1.add(
+                technologies.GasGridConnection(
+                    gas_type=gas,
+                    grid_pressure=template.input_pressure,
+                    working_rate=5,
+                )
+            )
+
+        house_1.add(
+            carriers.HeatCarrier(
+                temperature_levels=[20, template.maximum_temperature],
+                reference_temperature=10,
+            )
+        )
+
+        chp = CHP("chp", nominal_power=100e3, template=template)
+        self.check_chp_template(chp, template)
+        house_1.add(chp)
+
+        # Add heat demands
+        house_1.add(
+            demands.FixedTemperatureHeating(
+                name="heat_demand",
+                min_flow_temperature=template.maximum_temperature,
+                return_temperature=template.minimum_temperature,
+                time_series=[1000],
+            )
+        )
+
+        house_1.add(
+            demands.Electricity(
+                name="electricity_demand",
+                time_series=[1000],
+            )
+        )
+
+        solph_representation = SolphModel(
+            energy_system,
+            timeindex={
+                "start": "2022-06-01 08:00:00",
+                "end": "2022-06-01 09:00:00",
+                "freq": "60T",
+                "tz": "Europe/Berlin",
+            },
+        )
+
+        solph_representation.build_solph_model()
+        solved_model = solph_representation.solve(solve_kwargs={"tee": False})        
+        mr = meta_results(solved_model)
+        assert math.isclose(expected_result, mr["objective"], abs_tol=3e-3)
+        
+        # TODO: make sure gas is being imported
+        # TODO: make sure electricity is being produced
+        # TODO: make sure electricity is not being imported
+        # TODO: make sure heat is being produced
+        
+# class TestOffsetCHP:
+
+#     def check_chp_template(self, chp: CHP, template: CHPTemplate):
+
+#         assert chp.full_load_electrical_efficiency == template.full_load_electrical_efficiency
+#         assert chp.full_load_thermal_efficiency == template.full_load_thermal_efficiency
+#         assert chp.min_load_electrical_efficiency == template.min_load_electrical_efficiency
+#         assert chp.min_load_thermal_efficiency == template.min_load_thermal_efficiency
+#         assert chp.maximum_temperature == template.maximum_temperature
+#         assert chp.minimum_temperature == template.minimum_temperature
+#         assert chp.input_pressure == template.input_pressure
+#         assert chp.gas_type == template.gas_type
+#         assert type(chp.gas_type) is dict
+
+#     @pytest.mark.parametrize(
+#         "template, expected_result",
+#         [(NATURALGAS_CHP, 1466.5878210550002), 
+#          (BIOGAS_CHP, 966.562821055), 
+#          (BIOMETHANE_CHP, 0.5883656500000001), 
+#          (HYDROGEN_CHP, 0.5883656500000001), 
+#          (HYDROGEN_MIXED_CHP,  0.557825485)],
+#     )
+#     def test_chp(self, template, expected_result):
+
+#         os.chdir(os.path.dirname(__file__))
+#         energy_system = MetaModel()
+#         house_1 = Location(name="house_1")
+#         energy_system.add_location(house_1)
+
+#         house_1.add(carriers.ElectricityCarrier())
+#         house_1.add(technologies.ElectricityGridConnection(working_rate=50e-6))
+
+#         house_1.add(
+#             carriers.GasCarrier(
+#                 gases={
+#                     HYDROGEN: [template.input_pressure],
+#                 }
+#             )
+#         )
+
+#         house_1.add(
+#             carriers.HeatCarrier(
+#                 temperature_levels=[20, template.maximum_temperature],
+#                 reference_temperature=10,
+#             )
+#         )
+
+#         fc = CHP("chp", nominal_power=10e3, template=template)
+#         self.check_chp_template(fc, template)
+#         house_1.add(fc)
+
+#         house_1.add(
+#             technologies.GasGridConnection(
+#                 gas_type=HYDROGEN,
+#                 grid_pressure=template.input_pressure,
+#                 working_rate=5,
+#             )
+#         )
+
+#         # Add heat demands
+#         house_1.add(
+#             demands.FixedTemperatureHeating(
+#                 name="heat_demand",
+#                 min_flow_temperature=template.maximum_temperature,
+#                 return_temperature=template.minimum_temperature,
+#                 time_series=[1000],
+#             )
+#         )
+
+#         house_1.add(
+#             demands.Electricity(
+#                 name="electricity_demand",
+#                 time_series=[1000],
+#             )
+#         )
+
+#         solph_representation = SolphModel(
+#             energy_system,
+#             timeindex={
+#                 "start": "2022-06-01 08:00:00",
+#                 "end": "2022-06-01 09:00:00",
+#                 "freq": "60T",
+#                 "tz": "Europe/Berlin",
+#             },
+#         )
+
+#         solph_representation.build_solph_model()
+#         solved_model = solph_representation.solve(solve_kwargs={"tee": False})
+#         mr = meta_results(solved_model)
+#         assert math.isclose(expected_result, mr["objective"], abs_tol=3e-3)
