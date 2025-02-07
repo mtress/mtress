e518c625265917ec75909b1d7ee627daeab59e7f
Julius Ellermann <julius.ellermann@dlr.de>
2025-02-07 13:28:23 +0100

    carriers can be added manually with a carrier specific penalty

--- src/mtress/technologies/_slack.py
+++ src/mtress/technologies/_slack.py
@@ -9,12 +9,14 @@ SPDX-FileCopyrightText: Julius Ellermann
 
 SPDX-License-Identifier: MIT
 """
+from typing import Dict
 
 from oemof.solph import Bus, Flow
 from oemof.solph.components import Source, Sink
 
 from .._abstract_component import AbstractSolphRepresentation
 from .._data_handler import TimeseriesSpecifier, TimeseriesType
+from ..carriers._abstract_carrier import AbstractCarrier
 from ..carriers import ElectricityCarrier, HeatCarrier, GasCarrier
 from ._abstract_technology import AbstractTechnology
 
@@ -22,31 +24,63 @@ from ._abstract_technology import AbstractTechnology
 class Slack(AbstractTechnology, AbstractSolphRepresentation):
     """A component that provides sink and source slack nodes"""
 
-    def __init__(self, penalty: float = 1e9):
+    def __init__(self, penalty: float | Dict[type, AbstractCarrier] = 1e9):
         """
         Initialize slack component with infinite source and sink
 
         :param penalty: assign a cost for each unit of missing / excess
-            energy (in any currency)
+            energy (in any currency) | per carrier
+            (CarrierClass[AbstractCarrier]: penalty[float])
         """
         super().__init__(name=self.__class__.__name__)
-        self.penalty = penalty
+        if type(penalty) == float:
+            # set same penalty for all presen carriers
+            self.penalty = penalty
+            self.auto_connect = True
+        elif type(penalty) == dict:
+            # check for correct dict structure
+            if all(
+                issubclass(k, AbstractCarrier) and type(v) == float
+                for k, v in penalty.items()
+            ):
+                self.penalty = penalty
+                self.auto_connect = False
+            else:
+                raise ValueError("Specify a penalty for each carrier!")
+
+    def try_carrier(self, carrier_type: type) -> AbstractCarrier | None:
+        try:
+            carrier = self.location.get_carrier(carrier_type)
+        except KeyError:
+            carrier = None
+        return carrier
 
     def build_core(self):
         """Build oemof solph core structure."""
-        # get all carriers
-        try:
-            electricity_carrier = self.location.get_carrier(ElectricityCarrier)
-        except KeyError:
-            electricity_carrier = None
-        try:
-            heat_carrier = self.location.get_carrier(HeatCarrier)
-        except KeyError:
-            heat_carrier = None
-        try:
-            gas_carrier = self.location.get_carrier(GasCarrier)
-        except KeyError:
-            gas_carrier = None
+        electricity_carrier = None
+        heat_carrier = None
+        gas_carrier = None
+
+        if self.auto_connect:
+            # get all carriers
+            electricity_carrier = self.try_carrier(ElectricityCarrier)
+            electricity_penalty = self.penalty
+            heat_carrier = self.try_carrier(HeatCarrier)
+            heat_penalty = self.penalty
+            gas_carrier = self.try_carrier(GasCarrier)
+            gas_penalty = self.penalty
+        else:
+            # get only the specified carriers
+            for k, v in self.penalty.items():
+                if k == ElectricityCarrier:
+                    electricity_carrier = self.try_carrier(ElectricityCarrier)
+                    electricity_penalty = v
+                elif k == HeatCarrier:
+                    heat_carrier = self.try_carrier(HeatCarrier)
+                    heat_penalty = v
+                elif k == GasCarrier:
+                    gas_carrier = self.try_carrier(GasCarrier)
+                    gas_penalty = v
 
         slack_source = {}  # missing energy
         slack_sink = {}  # excess energy
@@ -55,24 +89,24 @@ class Slack(AbstractTechnology, AbstractSolphRepresentation):
         # electricity: distribution node
         if electricity_carrier is not None:
             node = electricity_carrier.distribution
-            slack_source[node] = Flow(variable_costs=self.penalty)
-            slack_sink[node] = Flow(variable_costs=self.penalty)
+            slack_source[node] = Flow(variable_costs=electricity_penalty)
+            slack_sink[node] = Flow(variable_costs=electricity_penalty)
 
         # heat: all T_?? nodes
         if heat_carrier is not None:
             for h_node in heat_carrier.level_nodes.values():
-                slack_source[h_node] = Flow(variable_costs=self.penalty)
-                slack_sink[h_node] = Flow(variable_costs=self.penalty)
+                slack_source[h_node] = Flow(variable_costs=heat_penalty)
+                slack_sink[h_node] = Flow(variable_costs=heat_penalty)
 
         # gas: highest (missing) and lowest (excess) pressure per gas
         if gas_carrier is not None:
             for gas in gas_carrier.distribution.values():
                 gas_nodes = list(gas.values())  # always sorted
                 gas_high = gas_nodes[-1]
-                slack_source[gas_high] = Flow(variable_costs=self.penalty)
+                slack_source[gas_high] = Flow(variable_costs=gas_penalty)
 
                 gas_low = gas_nodes[0]
-                slack_sink[gas_low] = Flow(variable_costs=self.penalty)
+                slack_sink[gas_low] = Flow(variable_costs=gas_penalty)
 
         # create slack nodes
         self.create_solph_node(
