259a99a793538bcc33a7f59e98aba84a432cfe53
Pedro L. Magalhães <pedro.magalhaes@dlr.de>
2025-02-17 15:32:13 +0100

    Adds tests to make sure nominal and part-load performance differences matter.

--- tests/unit_tests/test_technologies/test_chp.py
+++ tests/unit_tests/test_technologies/test_chp.py
@@ -10,8 +10,7 @@ from mtress import (
     demands,
     technologies,
 )
-
-# from mtress.physics import NATURAL_GAS, BIOGAS, BIO_METHANE, HYDROGEN
+    
 from mtress.technologies._chp import CHPTemplate
 from mtress.technologies import (
     CHP,
@@ -48,14 +47,14 @@ class TestCHP:
     @pytest.mark.parametrize(
         "template, expected_result, allow_exports",
         [
-            # exports on @ net metering
+            # exports on @ net metering: electricity production is no problem
             (NATURALGAS_CHP, 0.8508048112500001, True),
             (BIOGAS_CHP, 1.6856640186499998, True),
             (BIOMETHANE_CHP, 0.7855700564999999, True),
             (HYDROGEN_CHP, 0.32534810449999996, True),
             (HYDROGEN_MIXED_CHP, 0.6963973715, True),
             (AET100NG_CHP, 0.8077001939999999, True),
-            # exports off: changes outcome when the electrical eff. is higher
+            # exports off: electricity production determines the rest
             (NATURALGAS_CHP, 0.8508048112500001, False),
             (BIOGAS_CHP, 58187714401.612885, False), # mismatched production!
             (BIOMETHANE_CHP, 0.7855700564999999, False),
@@ -338,6 +337,8 @@ class TestOffsetCHP:
              1.5, 0.09135757400000062+NATURALGAS_CHP.nominal_power*0.5*50e-6),
             # CHP cannot be used, electricity must be imported
             (NATURALGAS_CHP, 0.05, NATURALGAS_CHP.nominal_power*0.05*50e-6),
+            # CHP cannot be used, electricity must be imported
+            (NATURALGAS_CHP, 0.099, NATURALGAS_CHP.nominal_power*0.099*50e-6),
         ],
     )
     def test_power_limits(self,  template, load_multiplier, expected_result):
@@ -429,156 +430,196 @@ class TestOffsetCHP:
     # *************************************************************************
     # *************************************************************************
     
-    # TODO: make sure the efficiencies are being correctly used
-        
-    # @pytest.mark.parametrize(
-    #     "min_load_thermal_efficiency, "+
-    #     "nominal_thermal_efficiency, " +
-    #     "min_load_electrical_efficiency, "+
-    #     "nominal_electrical_efficiency, "+
-    #     "test_factor, "+
-    #     "expected_result",
-    #     [
-    #         # constant thermal efficiency, constant electrical efficiency
-    #         (0.45, 0.45, 0.35, 0.35, 1, 1.20879121),
-    #         (0.45, 0.45, 0.35, 0.35, 0.99, 1681318701.2087913),
-    #         # constant thermal efficiency, increasing electrical efficiency
-    #         (0.45, 0.45, 0.30, 0.35, 1, 1.22697395487),
-    #         (0.45, 0.45, 0.30, 0.35, 0.99, 1.225751159125),
-    #         # constant thermal efficiency, decreasing electrical efficiency
-    #         (0.45, 0.45, 0.35, 0.30, 1, 28021978301.409424),
-    #         (0.45, 0.45, 0.35, 0.30, 0.99, 29703296501.409424),
-    #         # # increasing thermal efficiency, constant electrical efficiency
-    #         # (0.40, 0.45, 0.35, 0.35, 1, 1.22697395487),
-    #         # (0.40, 0.45, 0.35, 0.35, 0.99, 1.225751159125),
-    #         # # decreasing thermal efficiency, constant electrical efficiency
-    #         # (0.45, 0.40, 0.35, 0.35, 1, 1.22697395487),
-    #         # (0.45, 0.40, 0.35, 0.35, 0.99, 1.225751159125),
+    @pytest.mark.parametrize(
+        "thermal_efficiency_variation, "+
+        "electrical_efficiency_variation, "+
+        "test_factor, "+
+        "expected_result",
+        [
+            # constant thermal efficiency, constant electrical efficiency
+            # - same performance on both time steps
+            (0.0, 0.0, 1, -1098.73620879), 
+            # - penalties on second time step because load is too low
+            (0.0, 0.0, 0.99,  1681317601.2637913), 
             
-    #         # # thermal efficiency increases, electrical efficiency increases
-    #         # (0.40, 0.45, 0.30, 0.35, 1, 28021978301.409424),
-    #         # (0.40, 0.45, 0.30, 0.35, 0.99, 29703296501.409424),
-    #         # # thermal efficiency decreases, electrical efficiency increases
-    #         # (0.45, 0.35, 0.30, 0.35, 1, 21794871601.409424),
-    #         # (0.45, 0.35, 0.30, 0.35, 0.99, 23102563701.409424),
-    #         # # thermal efficiency increases, electrical efficiency decreases
-    #         # (0.40, 0.45, 0.35, 0.30, 1, 1.229434300945),
-    #         # (0.40, 0.45, 0.35, 0.30, 0.99, 1.22803928158),
-    #         # # thermal efficiency decreases, electrical efficiency decreases
-    #         # (0.45, 0.35, 0.35, 0.30, 1, 1.222764685435),
-    #         # (0.45, 0.35, 0.35, 0.30, 0.99, 1.221836545355),
-    #     ],
-    # )
-    # def test_variable_efficiency(
-    #         self,
-    #         min_load_thermal_efficiency: float,
-    #         nominal_thermal_efficiency: float,
-    #         min_load_electrical_efficiency: float,
-    #         nominal_electrical_efficiency: float,
-    #         test_factor: float,
-    #         expected_result: float
-    #         ):
+            # constant thermal efficiency, increasing electrical efficiency
+            # - higher (worse) result because efficiency is reduced at low load
+            (0.0, -0.05, 1, -1084.4512090757), 
+            # - higher (worse) result because efficiency is reduced at low load
+            (0.0, -0.05, 0.99, 1681317615.548791), 
+            
+            # constant thermal efficiency, decreasing electrical efficiency
+            # - better results due to higher elec. efficiency at low load
+            (0.0, 0.05, 1, -1113.0212045045002),
+            # - better results due to higher electric. efficiency at low load
+            (0.0, 0.05, 0.99, 1681317586.9787953),
+            
+            # increasing thermal efficiency, constant electrical efficiency
+            # - same results since production matches demand
+            (-0.05, 0.0, 1, -1098.73620879),
+            # - penalties are lower since load is lower
+            (-0.05, 0.0, 0.99, 1494504381.2637913),
+            # decreasing thermal efficiency, constant electrical efficiency
+            # - same results since production matches demand
+            (0.05, 0.0, 1, -1098.73620879),
+            # - penalties are higher since load is higher
+            (0.05, 0.0, 0.99, 1868130721.2637913),
+            
+            # varying thermal and electrical efficiencies
+            # thermal efficiency increases, electrical efficiency increases
+            (-0.05, -0.05, 1, -1084.4512090757),
+            (-0.05, -0.05, 0.99, 1494504395.548791),
+            # thermal efficiency decreases, electrical efficiency increases
+            (0.05, -0.05, 1, -1084.4512090757),
+            (0.05, -0.05, 0.99, 1868130735.548791),
+            # thermal efficiency increases, electrical efficiency decreases
+            (-0.05, 0.05, 1, -1113.0212045045002),
+            (-0.05, 0.05, 0.99, 1494504366.9787953),
+            # thermal efficiency decreases, electrical efficiency decreases
+            (0.05, 0.05, 1, -1113.0212045045002),
+            (0.05, 0.05, 0.99, 1868130706.9787953),
+        ],
+    )
+    def test_variable_efficiency(
+            self,
+            thermal_efficiency_variation: float,
+            electrical_efficiency_variation: float,
+            test_factor: float,
+            expected_result: float
+            ):
         
-    #     allow_exports = True
-    #     template = NATURALGAS_CHP
+        # test details:
+        # - electrical loads are always set to the respect. nominal and minimum
+        # - a test factor of 1 requires exactly the nominal and minimum thermal
+        # loads;
+        # - a test factor below 1 requires less than the minimum thermal load,
+        # prompting penalties, since the unit is forced to produce extra heat
+        # - export prices are deliberately attractive so as not to create elec.
+        # load problems (loads are met through imports, exports gen. revenue)
+        # - the nominal thermal and electrical efficincies are kept constant
+        # since they determine the size of the loads (changing them would make
+        # interpreting the results harder)
+        # - runs with a test factor of 1 cannot have penalties and the
+        # differences reflect the impact of the minimum load perfomance differ.
         
-
-    #     os.chdir(os.path.dirname(__file__))
-    #     energy_system = MetaModel()
-    #     house_1 = Location(name="house_1")
-    #     energy_system.add_location(house_1)
-
-    #     house_1.add(carriers.ElectricityCarrier())
-    #     house_1.add(
-    #         technologies.ElectricityGridConnection(
-    #             working_rate=50e-6,
-    #             revenue=50e-6 if allow_exports else None
-    #             )
-    #         )
-
-    #     house_1.add(
-    #         carriers.GasCarrier(
-    #             gases={
-    #                 gas_label: [template.input_pressure]
-    #                 for gas_label, gas in template.gas_type.items()
-    #             }
-    #         )
-    #     )
-    #     for gas, share in template.gas_type.items():
-    #         house_1.add(
-    #             technologies.GasGridConnection(
-    #                 gas_type=gas,
-    #                 grid_pressure=template.input_pressure,
-    #                 working_rate=5,
-    #             )
-    #         )
-
-    #     house_1.add(
-    #         carriers.HeatCarrier(
-    #             temperature_levels=[
-    #                 template.minimum_temperature, 
-    #                 template.maximum_temperature
-    #                 ],
-    #             reference_temperature=template.minimum_temperature-10,
-    #         )
-    #     )
-    #     # 
-    #     nominal_power = 1000
-    #     chp = OffsetCHP(
-    #         name="chp", 
-    #         nominal_power=nominal_power,
-    #         allow_electricity_feed_in=allow_exports,
-    #         # override template
-    #         nominal_electrical_efficiency=nominal_electrical_efficiency,
-    #         nominal_thermal_efficiency=nominal_thermal_efficiency,
-    #         min_load_electrical_efficiency=min_load_electrical_efficiency,
-    #         min_load_thermal_efficiency=min_load_thermal_efficiency,
-    #         template=template
-    #         )
-    #     house_1.add(chp)
-
-    #     # Add heat demands
-    #     house_1.add(
-    #         demands.FixedTemperatureHeating(
-    #             name="heat_demand",
-    #             min_flow_temperature=template.maximum_temperature,
-    #             return_temperature=template.minimum_temperature,
-    #             time_series=[
-    #                 (nominal_power/nominal_electrical_efficiency)*
-    #                 nominal_thermal_efficiency,
-    #                 # the test factor is meant to adjust the demand
-    #                 (nominal_power*template.normalised_min_load/
-    #                  min_load_electrical_efficiency)*
-    #                 min_load_thermal_efficiency*test_factor,
-    #                 ],
-    #         )
-    #     )
+        nominal_thermal_efficiency = 0.45
+        nominal_electrical_efficiency = 0.35
+        min_load_thermal_efficiency = (
+            nominal_thermal_efficiency+thermal_efficiency_variation
+            )
+        min_load_electrical_efficiency = (
+            nominal_electrical_efficiency+electrical_efficiency_variation
+            )
         
-    #     house_1.add(
-    #         demands.Electricity(
-    #             name="electricity_demand",
-    #             time_series=[
-    #                 nominal_power, 
-    #                 nominal_power*template.normalised_min_load
-    #                 ],
-    #         )
-    #     )
+        allow_exports = True
+        template = NATURALGAS_CHP
+        # the nominal (electrical) power is set to 1000 to simplify the analys.
+        nominal_power = 1000
+        
+        price_imp_elec = 50e-6
+        price_exp_elec = 1 # exporting is the priority
+        price_imp_gas = 5
+        
+        max_fuel_power = nominal_power/nominal_electrical_efficiency
+        min_fuel_power = max_fuel_power*template.normalised_min_load
+        max_heat_power = max_fuel_power*nominal_thermal_efficiency
+        min_heat_power = min_fuel_power*min_load_thermal_efficiency
+        max_elec_power = nominal_power
+        min_elec_power = min_fuel_power*min_load_electrical_efficiency
+        
+        # *********************************************************************
 
-    #     solph_representation = SolphModel(
-    #         energy_system,
-    #         timeindex={
-    #             "start": "2022-06-01 08:00:00",
-    #             "end": "2022-06-01 10:00:00",
-    #             "freq": "60T",
-    #             "tz": "Europe/Berlin",
-    #         },
-    #     )
+        energy_system = MetaModel()
+        house_1 = Location(name="house_1")
+        energy_system.add_location(house_1)
 
-    #     solph_representation.build_solph_model()
-    #     solved_model = solph_representation.solve(solve_kwargs={"tee": False}
-    # )
-    #     mr = meta_results(solved_model)
-    #     assert math.isclose(expected_result, mr["objective"], abs_tol=3e-3)
+        house_1.add(carriers.ElectricityCarrier())
+        house_1.add(
+            technologies.ElectricityGridConnection(
+                working_rate=price_imp_elec,
+                revenue=price_exp_elec if allow_exports else None
+                )
+            )
+
+        house_1.add(
+            carriers.GasCarrier(
+                gases={
+                    gas_label: [template.input_pressure]
+                    for gas_label, gas in template.gas_type.items()
+                }
+            )
+        )
+        for gas, share in template.gas_type.items():
+            house_1.add(
+                technologies.GasGridConnection(
+                    gas_type=gas,
+                    grid_pressure=template.input_pressure,
+                    working_rate=price_imp_gas,
+                )
+            )
+
+        house_1.add(
+            carriers.HeatCarrier(
+                temperature_levels=[
+                    template.minimum_temperature, 
+                    template.maximum_temperature
+                    ],
+                reference_temperature=10,
+            )
+        )
+        # 
+        
+        chp = OffsetCHP(
+            name="chp", 
+            nominal_power=nominal_power,
+            allow_electricity_feed_in=allow_exports,
+            # override template
+            nominal_electrical_efficiency=nominal_electrical_efficiency,
+            nominal_thermal_efficiency=nominal_thermal_efficiency,
+            min_load_electrical_efficiency=min_load_electrical_efficiency,
+            min_load_thermal_efficiency=min_load_thermal_efficiency,
+            template=template
+            )
+        house_1.add(chp)
+        
+        # Add heat demands
+        house_1.add(
+            demands.FixedTemperatureHeating(
+                name="heat_demand",
+                min_flow_temperature=template.maximum_temperature,
+                return_temperature=template.minimum_temperature,
+                time_series=[
+                    max_heat_power,
+                    # the test factor is meant to adjust the demand
+                    min_heat_power*test_factor,
+                    ],
+            )
+        )
+        
+        house_1.add(
+            demands.Electricity(
+                name="electricity_demand",
+                time_series=[
+                    max_elec_power, 
+                    min_elec_power
+                    ],
+            )
+        )
+
+        solph_representation = SolphModel(
+            energy_system,
+            timeindex={
+                "start": "2022-06-01 08:00:00",
+                "end": "2022-06-01 10:00:00",
+                "freq": "60T",
+                "tz": "Europe/Berlin",
+            },
+        )
+
+        solph_representation.build_solph_model()
+        solved_model = solph_representation.solve(solve_kwargs={"tee": False})
+        mr = meta_results(solved_model)
+        assert math.isclose(expected_result, mr["objective"], abs_tol=1e-3)
 
 # *****************************************************************************
 # *****************************************************************************
