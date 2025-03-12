7c6e114b06dbd4ed41cc40ca55b3a99e76be0d2a
Ellermann <julius.ellermann@dlr.de>
2025-03-12 14:55:57 +0100

    rebase from dev

--- examples/electricity_heat/electricity_heat.py
+++ examples/electricity_heat/electricity_heat.py
@@ -105,15 +105,12 @@ solph_representation = SolphModel(
 
 solph_representation.build_solph_model()
 
-plot = solph_representation.graph(detail=True)
-plot.render(outfile="electricity_heat_detail.png")
-
-plot = solph_representation.graph(detail=False)
-plot.render(outfile="electricity_heat_simple.png")
-
-solved_model = solph_representation.solve(solve_kwargs={"tee": False})
+solved_model = solph_representation.solve(solve_kwargs={"tee": True})
 myresults = results(solved_model)
 flows = get_flows(myresults)
 
-plot = solph_representation.graph(detail=True, flow_results=flows)
-plot.render(outfile="electricity_heat_results.png")
+solved_model.write(
+    "electricity_heat.lp", io_options={"symbolic_solver_labels": True}
+)
+
+solph_representation.graph(flow_results=flows)
--- examples/electricity_heat/simple_heat.py
+++ examples/electricity_heat/simple_heat.py
@@ -54,16 +54,9 @@ solph_representation = SolphModel(
 
 solph_representation.build_solph_model()
 
-plot = solph_representation.graph(detail=True)
-plot.render(outfile="heat_detail.png")
-
-plot = solph_representation.graph(detail=False)
-plot.render(outfile="heat_simple.png")
-
-solved_model = solph_representation.solve(solve_kwargs={"tee": False})
+solved_model = solph_representation.solve(solve_kwargs={"tee": True})
 myresults = results(solved_model)
 flows = get_flows(myresults)
 
 
-plot = solph_representation.graph(detail=True, flow_results=flows)
-plot.render(outfile="heat_results.png")
+solph_representation.graph(flow_results=flows)
