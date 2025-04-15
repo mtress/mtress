9783b28afdc60cff9a8ac651244c4c37fe79fde7
Julius Ellermann <julius.ellermann@dlr.de>
2025-04-15 11:56:48 +0200

    node name to lower case

--- src/mtress/demands/_fixed_temperature_heat.py
+++ src/mtress/demands/_fixed_temperature_heat.py
@@ -203,7 +203,7 @@ class FixedTemperatureCooling(AbstractFixedTemperature):
         )
 
         self.create_solph_node(
-            label="Source",
+            label="source",
             node_type=Source,
             outputs={
                 input: Flow(
