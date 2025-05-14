6a85397f29652a75847f81b922caadc4cf60136f
Patrik Schönfeldt <patrik.schoenfeldt@dlr.de>
2025-05-14 11:33:01 +0200

    Adhere to maximum line length

--- examples/electricity_heat/electricity_heat_coll.py
+++ examples/electricity_heat/electricity_heat_coll.py
@@ -3,10 +3,10 @@ Basic working 'electricity_heat_coll' example.
 
 Basic working example which includes a location (house),
 electricity wise: an electricity carrier which acts as a electricity
-source/supply from the official grid (working price of 0.035 ct/Wh) which is solely
-used to supply the required electricity for an electric heater used as back up to the collector.
-And heat wise: a heat carrier, a solar collector as well as
-a heat demand time series.
+source/supply from the official grid (working price of 0.035 ct/Wh) which is
+solely used to supply the required electricity for an electric heater used as
+back up to the collector. And heat wise: a heat carrier, a solar collector as
+well as a heat demand time series.
 
 At first an energy system (here meta_model) is defined with a time series
 (index). Afterwards a location is defined and added to the energysystem. Then
@@ -69,29 +69,44 @@ house_1.add(electric_heater)
 
 # Yearly_rad = 1765e3 # data from Solarkeymark in Wh/m2a
 # Npro_output = 557e3 # data from npro in Wh/m2a
-Power_O_keymark = 115  # in W, 115 W for 10K temp diff; 767 W for 0K; Gb= 850, Gd= 150 and 2m2 coll area
+
+# in W, 115 W for
+#       10K temp diff
+#       767 W for 0K
+#       Gb= 850
+#       Gd= 150 and
+#       2m2 coll area
+Power_O_keymark = 115
 Acoll = 2  # in m2
 Rad_beam = 850  # in W/m2 total radiation, beam and diffuse
 Rad_diff = 150  # in W/m2 total radiation, beam and diffuse
 K_d = 0.827
 Rad_nom = 1350  # in W/m2 total radiation, beam and diffuse
+
 #######################
-# right now we underestimate slightly the non thermal gains and overestimate greatly the thermal losses - RECHECK the last ones!
+# right now we underestimate slightly the non thermal gains and overestimate
+# greatly the thermal losses - RECHECK the last ones!
 #######################
-house_1.add(  # parameters for the WISC coll (for npro validation) - nu0=0,329; a1=40,94, global radiation at 25° in Athens = 1985,48 kWh/m2a; Result (npro) =557,93 kWh/m2
+house_1.add(
+    # parameters for the WISC coll (for npro validation)
+    #       nu0=0,329
+    #       a1=40,94
+    #       global radiation at 25° in Athens = 1985,48 kWh/m2a
+    #       Result (npro) =557,93 kWh/m2
     technologies.HeatSource(
         name="thColl",
         reservoir_temperature=[15, 15],
         maximum_working_temperature=20,
         minimum_working_temperature=15,
         conductivity=40.94 * Acoll,
-        non_thermal_gains=0.329
-        * Acoll
-        * (
-            Rad_beam + K_d * Rad_diff
-        ),  # this factor is the nu_0 times the radiation (as time series in Wh/h) for the total collector area
-        nominal_power=Rad_nom
-        * Acoll,  # this factor has no influence - should be the maximum radiation on the collector plane for a given timestep (e.g. for hours in Wh/h) for the total collector area
+        # this factor is the nu_0 times the radiation
+        # (as time series in Wh/h) for the total collector area
+        non_thermal_gains=0.329 * Acoll * (Rad_beam + K_d * Rad_diff),
+        # this factor has no influence
+        # should be the maximum radiation on the collector plane
+        # for a given timestep (e.g. for hours in Wh/h)
+        # for the total collector area
+        nominal_power=Rad_nom * Acoll,
     )
 )
 
@@ -123,9 +138,6 @@ Qcoll = flows[
     ("house_1", "thColl", "source_reservoir"),
     ("house_1", "thColl", "heat_source"),
 ]
-# Qcoll_10 = flows [('house_1', 'thColl', 'heat_source'), ('house_1', 'thColl', 'source_10')].sum
-# Qcoll_15 = flows [('house_1', 'thColl', 'heat_source'), ('house_1', 'thColl', 'source_15')].sum
-# Qcoll_25 = flows [('house_1', 'thColl', 'heat_source'), ('house_1', 'thColl', 'source_25')]
 Overestimation = Qcoll - Power_O_keymark  # Npro_output
 
 
