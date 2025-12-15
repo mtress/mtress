95d987b318bcf61026914ef1e221cb97744aa881
Julius Ellermann <julius.ellermann@dlr.de>
2025-12-15 11:34:54 +0100

    color constants

--- src/mtress/__init__.py
+++ src/mtress/__init__.py
@@ -11,6 +11,10 @@ from ._location import Location
 from ._meta_model import Connection, MetaModel
 from ._solph_model import SolphModel
 
+ELECTRICITY_COLOR = "orange"
+GAS_COLOR = "steelblue"
+HEAT_COLOR = "maroon"
+
 __version__ = "3.0.0a5"
 
 __all__ = [
