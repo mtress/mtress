a9654e7cbe216faa7c6184b53c1cc392f7da0502
Julius Ellermann <julius.ellermann@dlr.de>
2026-01-05 14:47:13 +0100

    add energy type enum

--- src/mtress/_constants.py
+++ src/mtress/_constants.py
@@ -1,3 +1,13 @@
+from enum import IntEnum
+
+
+class EnergyType(IntEnum):
+    UNDEFINED = 0
+    ELECTRICITY = 1
+    HEAT = 2
+    GAS = 3
+
+
 ELECTRICITY_COLOR = "orange"
 GAS_COLOR = "steelblue"
 HEAT_COLOR = "maroon"
