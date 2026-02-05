f29c08ddbb2fc41132cbbed0ea04ca03f35a2097
Patrik Schönfeldt <patrik@salacia.snft.de>
2026-02-05 22:01:58 +0100

    Make ElectricityGridConnection a Node

--- src/mtress/technologies/grid_connection/_abstract_grid_connection.py
+++ src/mtress/technologies/grid_connection/_abstract_grid_connection.py
@@ -1,8 +1,26 @@
-from mtress._abstract_component import AbstractComponent
+from oemof.network.network.nodes import QualifiedLabel
+
+from ..._subnetwork import SubNetwork
 
 
-class AbstractGridConnection(AbstractComponent):
+class AbstractGridConnection(SubNetwork):
     """Abstract carrier class to ensure a unified interface."""
 
-    def __init__(self, **kwargs):
-        super().__init__(name=self.__class__.__name__, **kwargs)
+    def __init__(
+        self,
+        *,
+        parent_node=None,
+        custom_properties=None,
+    ):
+
+        label = self.__class__.__name__
+        if parent_node:
+            if isinstance(parent_node.label, QualifiedLabel):
+                label = QualifiedLabel([label, *parent_node.label])
+            else:
+                label = QualifiedLabel([label, parent_node.label])
+        super().__init__(
+            label=label,
+            parent_node=parent_node,
+            custom_properties=custom_properties,
+        )
