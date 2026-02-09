d1da6cb3ef89c361724158151acf00710f9e941f
Ellermann <julius.ellermann@dlr.de>
2026-02-09 11:23:45 +0100

    test all params

--- tests/unit_tests/demands/test_gas_demand.py
+++ tests/unit_tests/demands/test_gas_demand.py
@@ -17,6 +17,14 @@ def test_basic_initialisation():
     assert gd.label == "demand1"
     assert gd.parent == parent_node
     assert gd._time_series == [1, 2, 3]
+    assert gd.gas_type == HYDROGEN
+    assert gd.pressure == 40
 
     assert len(gd.inbound_interfaces) == 1
     assert len(gd.outbound_interfaces) == 0
+
+    print(gd.inbound_interfaces)
+    print(gd.outbound_interfaces)
+
+
+test_basic_initialisation()
