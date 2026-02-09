0b2deed29e7873d51bb6f06726dfd9d92204b255
Ellermann <julius.ellermann@dlr.de>
2026-02-09 08:20:52 +0100

    black

--- tests/unit_tests/demands/test_electricity_demand.py
+++ tests/unit_tests/demands/test_electricity_demand.py
@@ -2,13 +2,19 @@ from oemof.network import Node
 
 from mtress import demands
 
+
 def test_basic_initialisation():
 
     parent_node = Node("parent")
-    ed = demands.Electricity("demand1", location=parent_node, time_series=[1, 2, 3])
+    ed = demands.Electricity(
+        "demand1", location=parent_node, time_series=[1, 2, 3]
+    )
     assert ed.label == "demand1"
     assert ed.parent == parent_node
     assert ed._time_series == [1, 2, 3]
 
     assert len(ed.inbound_interfaces) == 1
     assert len(ed.outbound_interfaces) == 0
+
+
+test_basic_initialisation()
