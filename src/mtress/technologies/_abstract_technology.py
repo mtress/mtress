56c85fd055403fe465470c6dea9f6d4ce26821b0
Ellermann <julius.ellermann@dlr.de>
2026-04-17 13:40:23 +0200

    remove flow type - categorization never used

--- src/mtress/technologies/_abstract_technology.py
+++ src/mtress/technologies/_abstract_technology.py
@@ -1,22 +1,6 @@
 """Base class for MTRESS technologies."""
 
-from abc import ABC, abstractmethod
-from enum import Enum, auto
-
 from .._abstract_component import AbstractComponent
-
-
-class FlowType(Enum):
-    """Types/categories for oemof flows."""
-
-    ALL = auto()
-    IN = auto()
-    OUT = auto()
-    PRODUCTION = auto()
-    STORAGE = auto()
-    EXPORT = auto()
-    IMPORT = auto()
-    RENEWABLE = auto()
 
 
 class AbstractTechnology(AbstractComponent):
@@ -25,26 +9,3 @@ class AbstractTechnology(AbstractComponent):
     def __init__(self, **kwargs):
         """Initialize technology."""
         super().__init__(**kwargs)
-        self._flows = {flow_type: set() for flow_type in FlowType}
-
-    # TODO: The flow categorization logic should probably move to a solph
-    # specific class
-    def categorise_flow(self, flow, flow_types):
-        """Categorise given flow under the named flow_types."""
-        for flow_type in flow_types | {FlowType.ALL}:
-            if flow_type in self._flows:
-                self._flows[flow_type].add(flow)
-            else:
-                self._flows[flow_type] = {flow}
-
-    def get_flows(self, flow_types):
-        """Return flows categorised under all named flow_types."""
-        flows = self._flows[FlowType.ALL].copy()
-
-        for flow_type in flow_types:
-            try:
-                flows.intersection_update(self._flows[flow_type])
-            except KeyError:
-                return set()
-
-        return flows
