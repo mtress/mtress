f37fc0677db4b6e95a6f1778cbe6b0d7f3dd5507
Ellermann <julius.ellermann@dlr.de>
2026-04-17 10:56:35 +0200

    make demand nodes have full label

--- src/mtress/demands/_abstract_demand.py
+++ src/mtress/demands/_abstract_demand.py
@@ -1,5 +1,7 @@
 """Abstract demand class to ensure a unified interface."""
 
+from oemof.network.network.nodes import QualifiedLabel
+
 from .._subnetwork import SubNetwork
 
 
@@ -7,14 +9,15 @@ class AbstractDemand(SubNetwork):
     """
     Abstract demand class to ensure a unified interface.
     """
-    def __init__(
-            self,
-            label,
-            *,
-            location=None,
-            custom_properties=None):
+
+    def __init__(self, label, *, location=None, custom_properties=None):
+
+        if location:
+            if isinstance(location.label, QualifiedLabel):
+                label = QualifiedLabel([label, *location.label])
+            else:
+                label = QualifiedLabel([label, location.label])
+
         super().__init__(
-            label,
-            parent_node=location,
-            custom_properties=custom_properties
+            label, parent_node=location, custom_properties=custom_properties
         )
