3988bddde26ebc748881c998791e53afecf87f79
Ellermann <julius.ellermann@dlr.de>
2026-04-17 13:43:43 +0200

    make AbstractTechnology a SubNetwork

--- src/mtress/technologies/_abstract_technology.py
+++ src/mtress/technologies/_abstract_technology.py
@@ -1,11 +1,29 @@
-"""Base class for MTRESS technologies."""
+"""Abstract technology class to ensure a unified interface."""
 
-from .._abstract_component import AbstractComponent
+from oemof.network.network.nodes import QualifiedLabel
+
+from .._subnetwork import SubNetwork
 
 
-class AbstractTechnology(AbstractComponent):
-    """Base class for MTRESS technologies."""
+class AbstractTechnology(SubNetwork):
+    """Abstract technology class to ensure a unified interface."""
 
-    def __init__(self, **kwargs):
+    def __init__(
+        self,
+        *,
+        location=None,
+        custom_properties=None,
+    ):
         """Initialize technology."""
-        super().__init__(**kwargs)
+
+        label = self.__class__.__name__
+        if location:
+            if isinstance(location.label, QualifiedLabel):
+                label = QualifiedLabel([label, *location.label])
+            else:
+                label = QualifiedLabel([label, location.label])
+        super().__init__(
+            label,
+            parent_node=location,
+            custom_properties=custom_properties,
+        )
