92a5c2b40a6041f2eef47208cd50489e21025549
Julius Ellermann <julius.ellermann@dlr.de>
2026-04-20 09:38:10 +0200

    code formatting

--- src/mtress/demands/_abstract_demand.py
+++ src/mtress/demands/_abstract_demand.py
@@ -10,7 +10,13 @@ class AbstractDemand(SubNetwork):
     Abstract demand class to ensure a unified interface.
     """
 
-    def __init__(self, label, *, location=None, custom_properties=None):
+    def __init__(
+        self,
+        label,
+        *,
+        location=None,
+        custom_properties=None,
+    ):
 
         if location:
             if isinstance(location.label, QualifiedLabel):
@@ -19,5 +25,7 @@ class AbstractDemand(SubNetwork):
                 label = QualifiedLabel([label, location.label])
 
         super().__init__(
-            label, parent_node=location, custom_properties=custom_properties
+            label,
+            parent_node=location,
+            custom_properties=custom_properties,
         )
