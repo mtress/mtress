566945832791ac52b3384648a4d5c3738635f8ef
Julius Ellermann <julius.ellermann@dlr.de>
2026-04-22 10:30:30 +0200

    make abstract and electric heater a node

--- src/mtress/technologies/_heater.py
+++ src/mtress/technologies/_heater.py
@@ -1,91 +1,80 @@
 """This module provides simple heater components (X to heat)"""
 
-import logging
-
+from oemof.network.network.nodes import QualifiedLabel
 from oemof.solph import Bus, Flow, Investment
 from oemof.solph.components import Converter
 
+from .._constants import EnergyType
 from ..carriers import ElectricityCarrier, GasCarrier, HeatCarrier
 from ..physics import Gas
 from ._abstract_technology import AbstractTechnology
-
-from .._constants import EnergyType
-
-LOGGER = logging.getLogger(__file__)
 
 
 class AbstractHeater(AbstractTechnology):
     def __init__(
         self,
-        name: str,
+        label,
+        *,
         maximum_temperature: float,
         minimum_temperature: float,
+        specific_heat_capacity: float = 1.161,
+        location=None,
+        custom_properties=None,
     ):
         """
-        :param name: Set the name of the component.
+        :param label: Set the label of the component.
         :parma maximum_temperature: Temperature (in °C) of the heat output.
         :parma minimum_temperature: Lowest possible temperature (in °C)
             of the inlet.
         """
-        super().__init__(name=name)
-
-        self.maximum_temperature = maximum_temperature
-        self.minimum_temperature = minimum_temperature
-
-    def build_core(self):
-        """Build core structure of oemof.solph representation."""
-        super().build_core()
-
-        self.heat_bus = heat_bus = self.create_solph_node(
-            label="heat",
-            node_type=Bus,
+        super().__init__(
+            label,
+            location=location,
+            custom_properties=custom_properties,
         )
 
-        # Add heat connection
-        heat_carrier = self.location.get_carrier(HeatCarrier)
+        self._maximum_temperature = maximum_temperature
+        self._minimum_temperature = minimum_temperature
+        self._specific_heat_capacity = specific_heat_capacity
 
-        in_levels = heat_carrier.get_levels_between(
-            self.minimum_temperature, self.maximum_temperature
-        )
-        out_levels = heat_carrier.get_levels_between(
-            in_levels[1], self.maximum_temperature
+        # bookkeeping of heat i/o nodes
+        self._temp_nodes = {}
+
+    def _build_core(self):
+        self._heat_bus = self.subnode(
+            Bus,
+            local_name="heat",
         )
 
-        for temp_in, temp_out in zip(in_levels, out_levels):
-            bus_cold = heat_carrier.level_nodes[temp_in]
-            bus_warm = heat_carrier.level_nodes[temp_out]
-            self.create_solph_node(
-                label=f"heat_{temp_in:.0f}_{temp_out:.0f}",
-                node_type=Converter,
-                inputs={
-                    bus_cold: Flow(
-                        custom_properties={
-                            "unit": "kg/h",
-                            "energy_type": EnergyType.HEAT,
-                        }
-                    ),
-                    heat_bus: Flow(
-                        custom_properties={
-                            "unit": "W",
-                            "energy_type": EnergyType.HEAT,
-                        }
-                    ),
-                },
-                outputs={
-                    bus_warm: Flow(
-                        custom_properties={
-                            "unit": "kg/h",
-                            "energy_type": EnergyType.HEAT,
-                        }
-                    ),
-                },
-                conversion_factors={
-                    bus_warm: 1,
-                    bus_cold: 1,
-                    heat_bus: (temp_out - temp_in)
-                    * heat_carrier.specific_heat_capacity,
-                },
+        self._out_max_temp = self.subnode(
+            Bus,
+            local_name=f"output_{self._maximum_temperature}",
+            custom_properties={"temperature": self._maximum_temperature},
+        )
+        self._temp_nodes[self._maximum_temperature] = self._out_max_temp
+
+        self._in_min_temp = self.subnode(
+            Bus,
+            local_name=f"input_{self._minimum_temperature}",
+            custom_properties={"temperature": self._minimum_temperature},
+        )
+        self._temp_nodes[self._minimum_temperature] = self._in_min_temp
+
+        self.inbound_interfaces[EnergyType.HEAT] = [self._in_min_temp]
+        self.outbound_interfaces[EnergyType.HEAT] = [self._out_max_temp]
+
+    def get_temp_node(self, temp: float) -> Bus:
+        node = self._temp_nodes.get(temp)
+        if node is not None:
+            return node
+        else:
+            node = self.subnode(
+                Bus,
+                local_name=f"i/o_{temp}",
+                custom_properties={"temperature": temp},
             )
+            self._temp_nodes[temp] = node
+            return node
 
 
 class ResistiveHeater(AbstractHeater):
@@ -95,16 +84,19 @@ class ResistiveHeater(AbstractHeater):
 
     def __init__(
         self,
-        name: str,
+        label,
+        *,
         maximum_temperature: float,
         minimum_temperature: float = 0,
         thermal_power_limit: Investment | float = None,
         efficiency: float = 1,
+        location=None,
+        custom_properties=None,
     ):
         """
         Initialize ResistiveHeater.
 
-        :param name: Set the name of the component.
+        :param label: Set the label of the component.
         :param maximum_temperature: Temperature (in °C) of the heat output.
         :param minimum_temperature: Lowest possible temperature (in °C)
             of the inlet.
@@ -113,27 +105,36 @@ class ResistiveHeater(AbstractHeater):
         :param efficiency: Thermal conversion efficiency.
         """
         super().__init__(
-            name=name,
+            label,
             maximum_temperature=maximum_temperature,
             minimum_temperature=minimum_temperature,
+            location=location,
+            custom_properties=custom_properties,
         )
 
         self.thermal_power_limit = thermal_power_limit
         self.efficiency = efficiency
 
-    def build_core(self):
-        """Build core structure of oemof.solph representation."""
-        super().build_core()
+        self._build_core()
 
-        # Add electrical connection
-        electricity_carrier = self.location.get_carrier(ElectricityCarrier)
-        electrical_bus = electricity_carrier.distribution
+    def _build_core(self):
+        super()._build_core()
 
-        self.create_solph_node(
-            label="heater",
-            node_type=Converter,
+        # add technology specific inlet
+        self._in_electricity = self.subnode(
+            Bus,
+            local_name="input_electricity",
+        )
+        self.inbound_interfaces[EnergyType.ELECTRICITY] = [
+            self._in_electricity
+        ]
+
+        # technology specific converter
+        self.subnode(
+            Converter,
+            local_name="heater",
             inputs={
-                electrical_bus: Flow(
+                self._in_electricity: Flow(
                     custom_properties={
                         "unit": "W",
                         "energy_type": EnergyType.ELECTRICITY,
@@ -141,7 +142,7 @@ class ResistiveHeater(AbstractHeater):
                 )
             },
             outputs={
-                self.heat_bus: Flow(
+                self._heat_bus: Flow(
                     custom_properties={
                         "unit": "W",
                         "energy_type": EnergyType.HEAT,
@@ -150,10 +151,100 @@ class ResistiveHeater(AbstractHeater):
                 )
             },
             conversion_factors={
-                electrical_bus: 1,
-                self.heat_bus: self.efficiency,
+                self._in_electricity: 1,
+                self._heat_bus: self.efficiency,
             },
         )
+
+    def establish_interconnections(self):
+        if self.parent:
+            # get carriers
+            heat_carrier: HeatCarrier = self.parent.get_carrier(HeatCarrier)
+            electricity_carrier: ElectricityCarrier = self.parent.get_carrier(
+                ElectricityCarrier
+            )
+
+            # connect electricity
+            self._in_electricity.inputs[electricity_carrier.distribution] = (
+                Flow(
+                    custom_properties={
+                        "unit": "W",
+                        "energy_type": EnergyType.ELECTRICITY,
+                    }
+                )
+            )
+
+            if self._maximum_temperature not in heat_carrier.levels:
+                raise ValueError(
+                    "Maximum temperature must be a temperature level"
+                )
+            if (
+                self._specific_heat_capacity
+                != heat_carrier.specific_heat_capacity
+            ):
+                raise ValueError("Specific heat capacities need to match")
+
+            # get heat levels
+            in_levels = heat_carrier.get_levels_between(
+                self._minimum_temperature, self._maximum_temperature
+            )
+            out_levels = heat_carrier.get_levels_between(
+                in_levels[1], self._maximum_temperature
+            )
+
+            for temp_in, temp_out in zip(in_levels, out_levels):
+                hc_bus_cold = heat_carrier.level_nodes[temp_in]
+                hc_bus_warm = heat_carrier.level_nodes[temp_out]
+                bus_in = self.get_temp_node(temp_in)
+                bus_out = self.get_temp_node(temp_out)
+
+                # connect busses
+                bus_in.inputs[hc_bus_cold] = Flow(
+                    custom_properties={
+                        "unit": "kg/h",
+                        "energy_type": EnergyType.HEAT,
+                    }
+                )
+                bus_out.outputs[hc_bus_warm] = Flow(
+                    custom_properties={
+                        "unit": "kg/h",
+                        "energy_type": EnergyType.HEAT,
+                    }
+                )
+
+                # converter
+                self.subnode(
+                    Converter,
+                    local_name=f"heat_{temp_in:.0f}_{temp_out:.0f}",
+                    inputs={
+                        bus_in: Flow(
+                            custom_properties={
+                                "unit": "kg/h",
+                                "energy_type": EnergyType.HEAT,
+                            }
+                        ),
+                        self._heat_bus: Flow(
+                            custom_properties={
+                                "unit": "W",
+                                "energy_type": EnergyType.HEAT,
+                            }
+                        ),
+                    },
+                    outputs={
+                        bus_out: Flow(
+                            custom_properties={
+                                "unit": "kg/h",
+                                "energy_type": EnergyType.HEAT,
+                            }
+                        ),
+                    },
+                    conversion_factors={
+                        bus_out: 1,
+                        bus_in: 1,
+                        self._heat_bus: (temp_out - temp_in)
+                        * self._specific_heat_capacity,
+                    },
+                )
 
 
 class GasBoiler(AbstractHeater):
@@ -199,8 +290,7 @@ class GasBoiler(AbstractHeater):
         self.efficiency = efficiency
 
     def build_core(self):
-        """Build core structure of oemof.solph representation."""
-        super().build_core()
+        super()._build_core()
 
         gas_carrier = self.location.get_carrier(GasCarrier)
         _, pressure_level = gas_carrier.get_surrounding_levels(
