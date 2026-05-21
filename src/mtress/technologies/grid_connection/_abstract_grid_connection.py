f2dd71923544f7ffd00aa8599adbb90d4066ce13
Ellermann <julius.ellermann@dlr.de>
2026-05-21 07:33:51 +0200

    add default connect error handling

--- src/mtress/technologies/grid_connection/_abstract_grid_connection.py
+++ src/mtress/technologies/grid_connection/_abstract_grid_connection.py
@@ -2,6 +2,8 @@ from oemof.network.network.nodes import QualifiedLabel
 
 from ..._subnetwork import SubNetwork
 
+from abc import abstractmethod
+
 
 class AbstractGridConnection(SubNetwork):
     """Abstract carrier class to ensure a unified interface."""
@@ -24,3 +26,13 @@ class AbstractGridConnection(SubNetwork):
             parent_node=location,
             custom_properties=custom_properties,
         )
+
+    @abstractmethod
+    def _connect(self, other: AbstractGridConnection):
+        pass
+
+    def connect(self, other: AbstractGridConnection):
+        if self.__class__ != other.__class__:
+            raise TypeError("GridConnections need to be of same type")
+
+        self._connect(other=other)
