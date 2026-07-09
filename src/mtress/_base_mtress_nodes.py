dbbdbaa04dbc3e41a89e07e37aac6439c9c93591
Ellermann <julius.ellermann@dlr.de>
2026-07-09 11:22:09 +0200

    add AbstractGridConnection as base node

--- src/mtress/_base_mtress_nodes.py
+++ src/mtress/_base_mtress_nodes.py
@@ -84,3 +84,31 @@ class AbstractTechnology(SubNetwork):
             parent_node=parent_node,
             custom_properties=custom_properties,
         )
+
+
+class AbstractGridConnection(SubNetwork):
+    """Abstract technology class to ensure a unified interface."""
+
+    def __init__(
+        self,
+        label,
+        *,
+        parent_node=None,
+        custom_properties=None,
+    ):
+        """Initialize technology."""
+        super().__init__(
+            label,
+            parent_node=parent_node,
+            custom_properties=custom_properties,
+        )
+
+    @abstractmethod
+    def _connect(self, other: AbstractGridConnection):
+        pass
+
+    def connect(self, other: AbstractGridConnection):
+        if self.__class__ != other.__class__:
+            raise TypeError("GridConnections need to be of same type")
+
+        self._connect(other=other)
