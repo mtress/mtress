f5b177943d1ac61f00b40ea31665d179750f5c02
Julius Ellermann <julius.ellermann@dlr.de>
2026-07-10 11:18:07 +0200

    test battery renewable electricity source

--- tests/integration_tests/technologies/test_renewable_electricity_source.py
+++ tests/integration_tests/technologies/test_renewable_electricity_source.py
@@ -1,43 +1,12 @@
-from oemof.solph import Results
-from oemof.visio import ESGraphRenderer
+from oemof.solph import Model
 
-from mtress import Location, MetaModel, SolphModel, carriers, demands
+from mtress import EnergySystem, Location, carriers, demands
 from mtress.technologies import RenewableElectricitySource, grid_connection
 
 
 def test_basic_initialisation():
 
-    energy_system = MetaModel()
-
-    loc = Location("house")
-
-    energy_system.add_location(loc)
-
-    ec = carriers.ElectricityCarrier(location=loc)
-
-    res1 = RenewableElectricitySource(
-        "pv1",
-        location=loc,
-        nominal_power=1,
-        specific_generation=[1, 2, 3],
-    )
-    assert res1.label == ("pv1", "house")
-    assert res1.parent == loc
-    assert res1._specific_generation == [1, 2, 3]
-    assert res1._fixed == True
-
-    assert len(res1.inbound_interfaces) == 0
-    assert len(res1.outbound_interfaces) == 1
-
-    ed1 = demands.Electricity("demand1", location=loc, time_series=[1, 2, 3])
-
-    egc = grid_connection.ElectricityGridConnection(
-        working_rate=0.3,
-        location=loc,
-    )
-
-    solph_representation = SolphModel(
-        energy_system,
+    energy_system = EnergySystem(
         timeindex={
             "start": "2021-07-10 00:00:00",
             "end": "2021-07-10 03:00:00",
@@ -45,20 +14,53 @@ def test_basic_initialisation():
         },
     )
 
-    solph_representation.graph(
+    loc = Location("house")
+
+    energy_system.add(loc)
+
+    ec = loc.subnode(
+        carriers.ElectricityCarrier,
+        local_name="EC",
+    )
+
+    res = loc.subnode(
+        RenewableElectricitySource,
+        local_name="pv",
+        nominal_power=1,
+        specific_generation=[1, 2, 3],
+    )
+
+    assert res.label == ("pv", "house")
+    assert res.parent == loc
+    assert res._specific_generation == [1, 2, 3]
+    assert res._fixed == True
+
+    assert len(res.inbound_interfaces) == 0
+    assert len(res.outbound_interfaces) == 1
+
+    ed = loc.subnode(
+        demands.Electricity,
+        local_name="demand",
+        time_series=[1, 2, 3],
+    )
+
+    energy_system.establish_interconnections()
+
+    energy_system.graph(
         path="tests/integration_tests/technologies/test_renewable_electricity_source.png"
     )
 
-    solved_model = solph_representation.solve(solve_kwargs={"tee": False})
-    myresults = Results(solved_model)
-    assert (
-        myresults._solver_results.Problem.lower_bound
-        == myresults._solver_results.Problem.upper_bound
-    )
-    assert myresults._solver_results.Problem.lower_bound == 0.0
+    model = Model(energy_system)
+    results = model.solve()
 
-    flows = myresults["flow"]
-    solph_representation.graph(
+    assert (
+        results._solver_results.Problem.lower_bound
+        == results._solver_results.Problem.upper_bound
+    )
+    assert results._solver_results.Problem.lower_bound == 0.0
+
+    flows = results["flow"]
+    energy_system.graph(
         flow_results=flows,
         path="tests/integration_tests/technologies/test_renewable_electricity_source_results.png",
     )
@@ -66,36 +68,7 @@ def test_basic_initialisation():
 
 def test_export():
 
-    energy_system = MetaModel()
-
-    loc = Location("house")
-
-    energy_system.add_location(loc)
-
-    ec = carriers.ElectricityCarrier(location=loc)
-
-    res1 = RenewableElectricitySource(
-        "pv1",
-        location=loc,
-        nominal_power=1,
-        specific_generation=[1, 2, 3],
-    )
-    assert res1.label == ("pv1", "house")
-    assert res1.parent == loc
-    assert res1._specific_generation == [1, 2, 3]
-    assert res1._fixed == True
-
-    assert len(res1.inbound_interfaces) == 0
-    assert len(res1.outbound_interfaces) == 1
-
-    egc = grid_connection.ElectricityGridConnection(
-        working_rate=0.3,
-        revenue=0.06,
-        location=loc,
-    )
-
-    solph_representation = SolphModel(
-        energy_system,
+    energy_system = EnergySystem(
         timeindex={
             "start": "2021-07-10 00:00:00",
             "end": "2021-07-10 03:00:00",
@@ -103,20 +76,54 @@ def test_export():
         },
     )
 
-    solph_representation.graph(
+    loc = Location("house")
+
+    energy_system.add(loc)
+
+    ec = loc.subnode(
+        carriers.ElectricityCarrier,
+        local_name="EC",
+    )
+
+    res = loc.subnode(
+        RenewableElectricitySource,
+        local_name="pv",
+        nominal_power=1,
+        specific_generation=[1, 2, 3],
+    )
+
+    assert res.label == ("pv", "house")
+    assert res.parent == loc
+    assert res._specific_generation == [1, 2, 3]
+    assert res._fixed == True
+
+    assert len(res.inbound_interfaces) == 0
+    assert len(res.outbound_interfaces) == 1
+
+    egc = loc.subnode(
+        grid_connection.ElectricityGridConnection,
+        local_name="EGC",
+        working_rate=0.3,
+        revenue=0.06,
+    )
+
+    energy_system.establish_interconnections()
+
+    energy_system.graph(
         path="tests/integration_tests/technologies/test_renewable_electricity_source_revenue.png"
     )
 
-    solved_model = solph_representation.solve(solve_kwargs={"tee": False})
-    myresults = Results(solved_model)
-    assert (
-        myresults._solver_results.Problem.lower_bound
-        == myresults._solver_results.Problem.upper_bound
-    )
-    assert myresults._solver_results.Problem.lower_bound == -0.36
+    model = Model(energy_system)
+    results = model.solve()
 
-    flows = myresults["flow"]
-    solph_representation.graph(
+    assert (
+        results._solver_results.Problem.lower_bound
+        == results._solver_results.Problem.upper_bound
+    )
+    assert results._solver_results.Problem.lower_bound == -0.36
+
+    flows = results["flow"]
+    energy_system.graph(
         flow_results=flows,
         path="tests/integration_tests/technologies/test_renewable_electricity_source_revenue_results.png",
     )
@@ -124,38 +131,7 @@ def test_export():
 
 def test_export_and_import():
 
-    energy_system = MetaModel()
-
-    loc = Location("house")
-
-    energy_system.add_location(loc)
-
-    ec = carriers.ElectricityCarrier(location=loc)
-
-    res1 = RenewableElectricitySource(
-        "pv1",
-        location=loc,
-        nominal_power=1,
-        specific_generation=[1, 2, 3],
-    )
-    assert res1.label == ("pv1", "house")
-    assert res1.parent == loc
-    assert res1._specific_generation == [1, 2, 3]
-    assert res1._fixed == True
-
-    assert len(res1.inbound_interfaces) == 0
-    assert len(res1.outbound_interfaces) == 1
-
-    ed1 = demands.Electricity("demand1", location=loc, time_series=[3, 2, 1])
-
-    egc = grid_connection.ElectricityGridConnection(
-        working_rate=0.3,
-        revenue=0.06,
-        location=loc,
-    )
-
-    solph_representation = SolphModel(
-        energy_system,
+    energy_system = EnergySystem(
         timeindex={
             "start": "2021-07-10 00:00:00",
             "end": "2021-07-10 03:00:00",
@@ -163,20 +139,59 @@ def test_export_and_import():
         },
     )
 
-    solph_representation.graph(
+    loc = Location("house")
+
+    energy_system.add(loc)
+
+    ec = loc.subnode(
+        carriers.ElectricityCarrier,
+        local_name="EC",
+    )
+
+    res = loc.subnode(
+        RenewableElectricitySource,
+        local_name="pv",
+        nominal_power=1,
+        specific_generation=[1, 2, 3],
+    )
+    assert res.label == ("pv", "house")
+    assert res.parent == loc
+    assert res._specific_generation == [1, 2, 3]
+    assert res._fixed == True
+
+    assert len(res.inbound_interfaces) == 0
+    assert len(res.outbound_interfaces) == 1
+
+    ed = loc.subnode(
+        demands.Electricity,
+        local_name="demand",
+        time_series=[3, 2, 1],
+    )
+
+    egc = loc.subnode(
+        grid_connection.ElectricityGridConnection,
+        local_name="EGC",
+        working_rate=0.3,
+        revenue=0.06,
+    )
+
+    energy_system.establish_interconnections()
+
+    energy_system.graph(
         path="tests/integration_tests/technologies/test_renewable_electricity_source_im_export.png"
     )
 
-    solved_model = solph_representation.solve(solve_kwargs={"tee": False})
-    myresults = Results(solved_model)
-    assert (
-        myresults._solver_results.Problem.lower_bound
-        == myresults._solver_results.Problem.upper_bound
-    )
-    assert myresults._solver_results.Problem.lower_bound == 0.48
+    model = Model(energy_system)
+    results = model.solve()
 
-    flows = myresults["flow"]
-    solph_representation.graph(
+    assert (
+        results._solver_results.Problem.lower_bound
+        == results._solver_results.Problem.upper_bound
+    )
+    assert results._solver_results.Problem.lower_bound == 0.48
+
+    flows = results["flow"]
+    energy_system.graph(
         flow_results=flows,
         path="tests/integration_tests/technologies/test_renewable_electricity_source_im_export_results.png",
     )
