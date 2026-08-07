354b57837d4e57d2ea978928e1d5a9ed0fc28e6f
Julius Ellermann <julius.ellermann@dlr.de>
2026-08-07 14:27:21 +0200

    simple electricity demand example

---
+++ examples/electricity_only/electricity_oemof_demand.py
@@ -0,0 +1,101 @@
+"""
+Basic working 'electricity only' example.
+
+Basic working 'electricity only' example which includes a location (house),
+an electricity carrier which acts as a electricity source/supply from the
+official grid (working price of 35 ct/kWh) as well as a demand (consumer)
+with a demand time series.
+
+At first an energy system is defined with a time series (index).
+Afterwards a location is defined and added to the energy system.
+Then the electricity carrier and demand (time series) are added to the
+energy system.
+"""
+
+import os
+
+from oemof.solph import Flow, Model
+from oemof.solph.components import Sink
+
+from mtress import (
+    EnergySystem,
+    EnergyType,
+    Location,
+    carriers,
+    demands,
+    technologies,
+)
+from mtress._helpers._visualization import graph_graphviz
+
+os.chdir(os.path.dirname(__file__))
+
+energy_system = EnergySystem(
+    timeindex={
+        "start": "2021-07-10 00:00:00",
+        "end": "2021-07-10 03:00:00",
+        "freq": "60min",
+    },
+)
+
+house_1 = Location(label="house_1")
+energy_system.add(house_1)
+
+ec: carriers.ElectricityCarrier = house_1.subnode(
+    carriers.ElectricityCarrier,
+    local_name="EC",
+)
+
+house_1.subnode(
+    technologies.ElectricityGridConnection,
+    "EGC",
+    working_rate=35,
+)
+
+house_1.subnode(
+    Sink,
+    local_name="sink 1",
+    inputs={
+        ec.distribution: Flow(
+            custom_properties={
+                "unit": "W",
+                "energy_type": EnergyType.ELECTRICITY,
+            },
+            nominal_capacity=1,
+            fix=[1, 0, -1],
+        )
+    },
+)
+
+house_1.subnode(
+    Sink,
+    local_name="sink 2",
+    inputs={
+        ec.distribution: Flow(
+            custom_properties={
+                "unit": "W",
+                "energy_type": EnergyType.ELECTRICITY,
+            },
+            nominal_capacity=1,
+            fix=[0, 1, 2],
+        )
+    },
+)
+
+energy_system.establish_interconnections()
+
+graph_graphviz(
+    energy_system.nodes,
+    path="electricity_oemof_demand.png",
+)
+
+model = Model(energy_system)
+
+myresults = model.solve()
+
+flows = myresults["flow"]
+
+graph_graphviz(
+    energy_system.nodes,
+    flows=flows,
+    path="electricity_oemof_demand_results.png",
+)
---
+++ examples/electricity_only/electricity_only.py
@@ -0,0 +1,70 @@
+"""
+Basic working 'electricity only' example.
+
+Basic working 'electricity only' example which includes a location (house),
+an electricity carrier which acts as a electricity source/supply from the
+official grid (working price of 35 ct/kWh) as well as a demand (consumer)
+with a demand time series.
+
+At first an energy system is defined with a time series (index).
+Afterwards a location is defined and added to the energy system.
+Then the electricity carrier and demand (time series) are added to the
+energy system.
+"""
+
+import os
+
+from oemof.solph import Model
+
+from mtress import EnergySystem, Location, carriers, demands, technologies
+from mtress._helpers._visualization import graph_graphviz
+
+os.chdir(os.path.dirname(__file__))
+
+energy_system = EnergySystem(
+    timeindex={
+        "start": "2021-07-10 00:00:00",
+        "end": "2021-07-10 02:00:00",
+        "freq": "60min",
+    },
+)
+
+house_1 = Location(label="house_1")
+energy_system.add(house_1)
+
+house_1.subnode(
+    carriers.ElectricityCarrier,
+    local_name="EC",
+)
+
+house_1.subnode(
+    technologies.ElectricityGridConnection,
+    "EGC",
+    working_rate=35,
+)
+
+house_1.subnode(
+    demands.Electricity,
+    "electricity demand 1",
+    time_series=[0, 0.5],
+)
+
+
+energy_system.establish_interconnections()
+
+graph_graphviz(
+    energy_system.nodes,
+    path="electricity_only.png",
+)
+
+model = Model(energy_system)
+
+myresults = model.solve()
+
+flows = myresults["flow"]
+
+graph_graphviz(
+    energy_system.nodes,
+    flows=flows,
+    path="electricity_only_results.png",
+)
