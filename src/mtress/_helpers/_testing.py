540b5fa0091fd22e3c302f1bdc84e14091e6f81d
Patrik Schönfeldt <Patrik.Schoenfeldt@dlr.de>
2026-08-12 16:10:44 +0200

    Move testing helpers to MTRESS src

--- src/mtress/_helpers/_testing.py
+++ src/mtress/_helpers/_testing.py
@@ -2,5 +2,5 @@
 
 def assert_every_element_is_same(iterable1, iterable2):
     assert len(iterable1) == len(iterable2)
-    for value1, value2 in iterable1, iterable2:
+    for value1, value2 in zip(iterable1, iterable2):
         assert (value1 == value2).all()
