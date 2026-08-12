a4f3cce4a49a605aedc940baafe974bb0b02ff44
Julius Ellermann <julius.ellermann@dlr.de>
2026-08-12 15:05:31 +0200

    Electricity specific Bus and Flow

---
+++ src/mtress/carriers/electricity/_electricity_flow.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""Flows to specify heat
+
+SPDX-FileCopyrightText: Deutsches Zentrum für Luft- und Raumfahrt e.V. (DLR)
+
+SPDX-License-Identifier: MIT
+"""
+
+from oemof.solph import Flow
+
+from mtress._energy_types import EnergyType
+
+
+class EnergyFlowElectricity(Flow):
+    def __init__(self, **kwargs):
+        if "custom_properties" not in kwargs:
+            kwargs["custom_properties"] = {}
+        kwargs["custom_properties"].update(
+            {
+                "unit": "W",
+                "energy_type": EnergyType.ELECTRICITY,
+            }
+        )
+        super().__init__(**kwargs)
