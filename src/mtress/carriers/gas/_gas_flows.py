c66ade83d0f573d8560033fa6f4ea0de7c871de7
Upadhaya <ajay.upadhaya@dlr.de>
2026-09-10 18:01:34 +0200

    Add gas flows

---
+++ src/mtress/carriers/gas/_gas_flows.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+"""
+Flows to specify pressure
+
+SPDX-FileCopyrightText: Deutsches Zentrum für Luft- und Raumfahrt e.V. (DLR)
+
+SPDX-License-Identifier: MIT
+"""
+
+from oemof.solph import Flow
+
+from mtress._energy_types import EnergyType
+
+class MassFlowGas(Flow):
+    def __init__(self, **kwargs):
+        if "custom_properties" not in kwargs:
+            kwargs["custom_properties"] = {}
+        kwargs["custom_properties"].update(
+            {
+                "unit": "kg/h",
+                "energy_type": EnergyType.GAS,
+            }
+        )
+        super().__init__(**kwargs)
+
+
+class EnergyFlowGas(Flow):
+    def __init__(self, **kwargs):
+        if "custom_properties" not in kwargs:
+            kwargs["custom_properties"] = {}
+        kwargs["custom_properties"].update(
+            {
+                "unit": "W",
+                "energy_type": EnergyType.GAS,
+            }
+        )
+        super().__init__(**kwargs)
